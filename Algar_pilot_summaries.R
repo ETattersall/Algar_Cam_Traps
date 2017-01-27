c8c361f3d31e8ae322144e4aad53ceb84afcecfe
ETattersall <ertattersall@gmail.com>
2017-01-27 11:45:31 -0800

    Updating pilot results, histograms for species detections and number of sites detected at.

--- Algar_pilot_summaries.R
+++ Algar_pilot_summaries.R
@@ -5,7 +5,14 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-## No. detections by species and station as data frames
+##Read in station data
+setwd(images_wd)
+cams2015 <- read.csv("Algar2015StationData.csv")
+
+## Plot station locations in utm
+with(cams2015,plot(utmE,utmN))
+
+## No. detections by species and station as data frames 
 spec_detect <- data.frame (rec.noTimelapse %>% count(Species))
 
 
@@ -13,7 +20,30 @@ Station_detect <- data.frame (rec.noTimelapse %>% count(Station))
 
 ### Or you can take it directly from rec.noTimelapse
 
-species_seen <- rec.noTimelapse$Species
+sp_detect <- rec.noTimelapse$Species
+st_detect <- rec.noTimelapse$Station
 
-species_hist <- ggplot(data.frame(species_seen), aes(x=species_seen)) +
-  geom_bar()
+### Frequency histograms, using Cole's code (Algar_pilot_Cole.R)
+
+sp.plot <- rev(sort(table(sp_detect))) 
+xvals <- barplot(sp.plot,names.arg = NA,col="royalblue4",ylab = "Camera detections",cex.lab=1.4,ylim=c(0,250))
+text(xvals,par("usr")[3]-0.25,srt=45,adj=1,labels=names(sp.plot),xpd=TRUE)
+
+# create Site x Species matrix
+S <- as.data.frame.matrix(table(st_detect,sp_detect))
+
+# species totals to compare with table(sp_detect)
+apply(S,2,sum)
+# number of sites per species
+sp.sites <- apply(S,2,function(x) sum(ifelse(x>0,1,0)))
+sp.plot2 <- rev(sort(sp.sites)) 
+xvals <- barplot(sp.plot2,names.arg = NA,col="royalblue4",ylab = "Number of sites",cex.lab=1.4)
+text(xvals,par("usr")[3]-0.25,srt=45,adj=1,labels=names(sp.plot2),xpd=TRUE)
+
+# add total count and total species (richness) for each site
+S$Total <- apply(S,1,sum)
+
+S$Richness <- apply(S[,1:17],1,function(x) sum(ifelse(x>0,1,0)))
+## Edit to Cole's code: S$Richness included total in Richness count. Adding [,1:17] to specify counting only species 
+## columns
+
