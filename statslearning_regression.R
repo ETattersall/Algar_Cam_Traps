7b96ed9ffd008c2eac9df668e727a410c9b12fb1
ETattersall <ertattersall@gmail.com>
2017-02-07 10:03:00 -0800

    Code for nonlinear terms, interactions, qualtitative terms, writing plotting functions

--- statslearning_regression.R
+++ statslearning_regression.R
@@ -40,3 +40,53 @@ par(mfrow = c(2,2))
 fit4 <- update(fit3, ~.-age-indus)
 summary(fit4)
 
+## Nonlinear terms and Interactions, * indicates interaction
+fit5 <- lm(medv~lstat*age, Boston)
+summary(fit5)
+
+## Adding a quadratic term
+fit6 <- lm(medv~lstat + I(lstat^2), Boston); summary(fit6)
+attach(Boston)
+par(mfrow = c(1,1))
+plot(medv~lstat)
+
+## Fit quadratic model to data
+points(lstat, fitted(fit6), col = "red", pch = 20)
+
+## Fitting polynomials with poly function  (overfitting the data)
+fit7 <- lm(medv~poly(lstat, 4)) ## 4 = degree of polynomial
+points(lstat,fitted(fit7), col = "blue", pch = 20)
+
+## Available plotting characters
+plot(1:20,1:20, pch = 1:20, cex = 2)
+
+
+## Qualitative predictors
+names(Carseats)
+summary(Carseats)
+## See that ShelveLoc, Urban, and US are qualitative variables
+
+fit1 <- lm(Sales~. + Income:Advertising + Age:Price, Carseats)
+summary(fit1)
+## Shows how qualitative measures are coded into dummy variables
+contrasts(Carseats$ShelveLoc)
+
+
+## Writing R functions
+## Fit a regression model, make a plot
+regplot <- function(x,y){
+  fit <- lm(y~x)
+  plot(x,y)
+  abline(fit, col = "red")
+}
+attach(Carseats)
+regplot(Price, Sales)
+
+## Adding unnamed arguments (...)
+regplot <- function(x,y,...){
+  fit <- lm(y~x)
+  plot(x,y,...)
+  abline(fit, col = "red")
+}
+
+regplot(Price, Sales, xlab = "Price", ylab = "Sales", col = "blue", pch = 20)
