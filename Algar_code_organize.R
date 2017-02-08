9441d6d6fb7e223adf789bac0adf18c2218b58b5
ETattersall <ertattersall@gmail.com>
2017-02-07 17:25:52 -0800

    Testing code for organizing by species, added everything up to loop for copying files into new Species_image directory

--- Algar_code_organize.R
+++ Algar_code_organize.R
@@ -185,50 +185,49 @@ A2$StudyDay <- floor(as.numeric(difftime(max(A2$Date.Time),min(A2$Date.Time),uni
 folder <- unique(A2$Folder)
 species <- c("O_virginianus","R_tarandus", "C_elavus", "A_alces", "C_lupus", "C_latrans", "U_americanus", "L_canadensis", "G_gulo", "M_americana", "M_pennanti", "V_vulpes", "T_hudsonicus", "L_americanus", "H_sapiens", "G_canadensis", "Other_birds", "Other")
 
+### Copy Raw image folder into new folder named "Species_images"
+Stat <- list.files("Raw_images")
+createStationFolders(inDir = "Species_images", 
+                     stations = Stat, 
+                     createinDir = TRUE)
+
+
+    
+
 ## Create a vector of paths to each station folder
-Station_wd <- c("C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_001", 
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_002", 
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_003",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_004", 
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_005",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_006",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_007",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_008",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_009",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_010",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_011",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_012",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_013",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_014",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_015",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_016",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_017",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_018",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_019",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_020",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_021",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_022",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_023",
-                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Renamed_images_Copy/Algar_024")
+Station_wd <- c("C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar1", 
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar2", 
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar3",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar4", 
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar5",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar6",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar7",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar8",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar9",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar10",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar11",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar12",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar13",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar14",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar15",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar16",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar17",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar18",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar19",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar20",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar21",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar22",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar23",
+                "C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar24")
 
 
 images_wd
 
 ### Create species folders
 
-SpecFolderCreate1 <- createSpeciesFolders (inDir               = "Renamed_images_Copy",
+SpecFolderCreate1 <- createSpeciesFolders (inDir               = "Species_images",
                                            species             = species,
                                            hasCameraFolders = FALSE,
                                            removeFolders       = FALSE)
-
-for (sp in species) {
-  setwd(Station_wd)
-  f_sp_fol <- spec_col %>%
-    filter(Folder == folder & Species == sp) %>% 
-    select(File)
-  vec_sp_fol <- unlist(f_sp_fol)
-  copySpeciesFiles <- file.copy(vec_sp_fol, sp)
-} 
-
 
 
