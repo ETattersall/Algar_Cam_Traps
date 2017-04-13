c5cb9dc6e28f321ce4807fb7c16719901b308af0
ETattersall <ertattersall@gmail.com>
2017-04-12 18:35:43 -0700

    Adding activity histograms for other target species

--- camtrapR_data_exploration.R
+++ camtrapR_data_exploration.R
@@ -6,7 +6,7 @@ library(camtrapR)
 ###Need record tables created in camtrapR_data_extraction
 
 ##Activity histograms
-Wolf_hist <- activityHistogram (recordTable = rec.spec.ind,
+Wolf_hist <- activityHistogram (recordTable = rec.spec,
                    species = "C_lupus",
                    allSpecies = FALSE,
                    speciesCol = "Species",
@@ -14,8 +14,24 @@ Wolf_hist <- activityHistogram (recordTable = rec.spec.ind,
                    recordDateTimeFormat = "%Y-%m-%d %H:%M:%S",
                    plotR = TRUE,
                    writePNG = FALSE)
+Bear_hist <- activityHistogram (recordTable = rec.spec,
+                                   species = "U_americanus",
+                                   allSpecies = FALSE,
+                                   speciesCol = "Species",
+                                   recordDateTimeCol = "DateTimeOriginal",
+                                   recordDateTimeFormat = "%Y-%m-%d %H:%M:%S",
+                                   plotR = TRUE,
+                                   writePNG = FALSE)
+Coyote_hist <- activityHistogram (recordTable = rec.spec,
+                                   species = "C_latrans",
+                                   allSpecies = FALSE,
+                                   speciesCol = "Species",
+                                   recordDateTimeCol = "DateTimeOriginal",
+                                   recordDateTimeFormat = "%Y-%m-%d %H:%M:%S",
+                                   plotR = TRUE,
+                                   writePNG = FALSE)
 
-Lynx_hist <- activityHistogram (recordTable = rec.spec.ind,
+Lynx_hist <- activityHistogram (recordTable = rec.spec,
                                 species = "L_canadensis",
                                 allSpecies = FALSE,
                                 speciesCol = "Species",
@@ -24,7 +40,7 @@ Lynx_hist <- activityHistogram (recordTable = rec.spec.ind,
                                 plotR = TRUE,
                                 writePNG = FALSE)
 
-Caribou_hist <- activityHistogram (recordTable = rec.spec.ind,
+Caribou_hist <- activityHistogram (recordTable = rec.spec,
                                 species = "R_tarandus",
                                 allSpecies = FALSE,
                                 speciesCol = "Species",
@@ -32,9 +48,25 @@ Caribou_hist <- activityHistogram (recordTable = rec.spec.ind,
                                 recordDateTimeFormat = "%Y-%m-%d %H:%M:%S",
                                 plotR = TRUE,
                                 writePNG = FALSE)
+WTdeer_hist <- activityHistogram (recordTable = rec.spec,
+                                   species = "O_virginianus",
+                                   allSpecies = FALSE,
+                                   speciesCol = "Species",
+                                   recordDateTimeCol = "DateTimeOriginal",
+                                   recordDateTimeFormat = "%Y-%m-%d %H:%M:%S",
+                                   plotR = TRUE,
+                                   writePNG = FALSE)
+Moose_hist <- activityHistogram (recordTable = rec.spec,
+                                   species = "A_alces",
+                                   allSpecies = FALSE,
+                                   speciesCol = "Species",
+                                   recordDateTimeCol = "DateTimeOriginal",
+                                   recordDateTimeFormat = "%Y-%m-%d %H:%M:%S",
+                                   plotR = TRUE,
+                                   writePNG = FALSE)
 ### activityDensity over 24 hr period --> not super useful?
 
-caribou.dens <- activityDensity(rec.spec.ind, 
+caribou.dens <- activityDensity(rec.spec, 
                 "R_tarandus",
                 allSpecies = FALSE,
                 speciesCol = "Species",
@@ -46,3 +78,14 @@ caribou.dens <- activityDensity(rec.spec.ind,
                 createDir = FALSE, 
                 pngMaxPix = 1000,
                 add.rug = TRUE)
+
+#### activityRadial
+activityRadial(recordTable       = rec.spec,
+               species           = "R_tarandus",
+               allSpecies        = FALSE,
+               speciesCol        = "Species",
+               recordDateTimeCol = "DateTimeOriginal",
+               plotR             = TRUE,
+               writePNG          = FALSE,
+               lwd               = 3
+)
