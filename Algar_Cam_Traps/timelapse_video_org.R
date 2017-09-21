b1aa59b577c69bec0883cc67c0df28090731542d
ETattersall <ertattersall@gmail.com>
2017-09-21 15:25:02 -0700

    Adding detailed comments

--- Algar_Cam_Traps/timelapse_video_org.R
+++ Algar_Cam_Traps/timelapse_video_org.R
@@ -1,10 +1,27 @@
 ### Copying Timelapse images into folders for video clips
-library(dplyr)
+## Need to read Timelapse images from master CSV, pull into a spearate folder (unique to that camera station)
+ 
+library(dplyr) ## Load for use of pipes
 
-setwd("C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Species_images/Algar_024")
 
-f_sp_fol <- master_csv %>%
-  filter(Folder == "Algar24" & Trigger_Mode == "Time Lapse") %>% 
+
+## Access data directory to read in csvs
+setwd("C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Data") 
+
+##Read in master CSV for 2016.01 deployment (Winter, snow only)
+CSVs.2016.01 <- read.csv("2016.01CSVs_06June_2017.csv", header = TRUE)
+
+## Subset data frame for Timelapse images for single camera station
+Time.14 <- CSVs.2016.01 %>%
+  filter(Folder == "ALGAR14" & TriggerMode == "T") %>% 
   select(File)
-vec_sp_fol <- unlist(f_sp_fol)
-file.copy(vec_sp_fol,"Algar_024_timelapse")
+
+## Set working directory to camera station
+setwd("C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/2016.01/Renamed_Images/Algar14")
+
+##Creates a vector of Timelapse image file names from list structure (list produced during subsetting above)
+vec14 <- unlist(Time.14)
+
+## Create a folder named "Algar13_timelapse" (done manually)
+## Copy Timelapse images into new folder (within that camera station)
+file.copy(vec14,"Algar14_timelapse")
