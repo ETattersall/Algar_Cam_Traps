b0d622609cb1853eecdcceaa3216848e429b6977
ETattersall <ertattersall@gmail.com>
2017-10-20 13:34:58 -0700

    aggregating duplicated months, adding sites with no detections

--- Algar_Cam_Traps/Algar_combine_datasets.R
+++ Algar_Cam_Traps/Algar_combine_datasets.R
@@ -17,6 +17,7 @@ win60$X <- NULL
 # Full dataset will have NAs for the months that Algar25-60 were inactive (Nov. 2015-Oct.2016)
 # 11 months, 36 cameras = 396 rows of NAs
 
+#### First 24 cameras only ####
 # Algar01 - 24 from 2nd deployment
 Alg24 <- win60[1:138, ]
 
@@ -26,5 +27,51 @@ alldat <- rbind(pilot,Alg24)
 ## Double counts for Nov. 2016 (counted in both surveys)?
 nov2016 <- alldat %>% filter(Yr_Month == "2016-11") ## Yes. Need to be combined
 
-## Use aggregate function?
+## Use aggregate function. Tested on data frame with only duplicated months
+nov2016 <- aggregate(. ~ Site + Treatment + Yr_Month + Site_ym, data = nov2016, sum) 
+
+### Combining data for 24 cams for full survey
+alldat <- alldat[!alldat$Yr_Month == "2016-11", ] #Remove duplicated rows
+
+alldat <- rbind(alldat,nov2016) #Combine fixed 2016-11 data to other months
+alldat <- alldat[with(alldat, order(Site, Yr_Month)), ] ## Ordering by Site and Yr_month
+
+## Algar18 failed in second deployment. Need to add NA rows?
+
+
+#### Full data set for all 60 cameras across all survey months ####
+## alldat = First 24 cameras for all survey months
+
+win36 <- win60[139:330, ] #Active cams. 192 rows --> Of the 36 additional cameras, 3 failed and 2 had no detections
+
+## Algar52 and Algar56 had no detections. Need to add rows for months they were active (Nov. 2016 - Apr. 2017)
+##Dataframe of 12x11
+# Site column
+Algar52 <- rep("Algar52",6)
+Algar56 <- rep("Algar56",6)
+Site <- append(Algar52,Algar56)
+
+# Treatment and Yr_Month columns
+Treatment <- rep("NatRegen", 12)
+Yr_Month <- rep(c("2016-11", "2016-12", "2017-01", "2017-02", "2017-03", "2017-04"),2)
+
+# Combining as a dataframe
+nodet <- as.data.frame(cbind(Site, Treatment, Yr_Month))
+# Site_ym column
+nodet$Site_ym <- paste(nodet$Site, nodet$Yr_Month)
+
+#Detection matrix full of 0's
+detections <- matrix(data = 0, nrow = 12, ncol = 7)
+colnames(detections) <- c("Blackbear", "Wolf", "Coyote", "Lynx", "Caribou", "WTDeer", "Moose")
+detections <- as.data.frame(detections)
+
+#Full data.frame for 0 detection sites
+nodet <- cbind(nodet,detections)
+
+#Add nodet to win36
+win36 <- as.data.frame(rbind(win36, nodet))
+win36 <- with(win36, win36[order(as.factor(as.character(Site)), Yr_Month), ]) ## Ordering by Site and Yr_month. as.factor(as.character()) rearranges the levels in Site
+tail(win36)
+
+##
 
