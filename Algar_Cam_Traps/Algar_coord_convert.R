cf0ac1a474540520b39e79aed98007b309bb6432
ETattersall <ertattersall@gmail.com>
2017-11-23 12:51:46 -0800

    Converting coordinates for Algar's offline sites (61-73)

--- Algar_Cam_Traps/Algar_coord_convert.R
+++ Algar_Cam_Traps/Algar_coord_convert.R
@@ -8,9 +8,13 @@ library(dplyr)
 library(rgdal)
 
 setwd("C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Data")
-Stat <- read.csv("Station_data/AlgarCameraStations_Nov2016.csv")
+Stat <- read.csv("Station_data/AlgarStations_SiteDesc.csv")
 head(Stat)
 colnames(Stat)
+
+##Separating out new stations, only utm data
+Stat <- Stat[61:73,]
+
 
 coords <- Stat %>% select(utmE, utmN)
 
@@ -20,10 +24,14 @@ spgeo <- spTransform(sputm, CRS("+proj=longlat +datum=WGS84"))
 class(spgeo)
 longlat <- as.data.frame(spgeo)
 colnames(longlat) <- c("Longitude", "Latitude")
-Stat <- cbind(Stat, longlat)
+Stat <- cbind(coords, longlat)
 head(Stat)
 colnames(Stat)
 
+write.csv(Stat, "Station_data/OffLine_coordinates.csv")
+
+
+## For Camelot Bulk Import prep
 Site.data <- Stat %>% select(CamStation, utmE, utmN, Longitude, Latitude, DeployDate, DeployTime_24h, Treatment, Elev_m, CamHeight_cm, CamDirection)
 colnames(Site.data)
 glimpse(Site.data)
