e616740cb4f9f6074efcefb3f98a390b20c0adb7
ETattersall <ertattersall@gmail.com>
2017-12-08 11:55:43 -0800

    Modified graphing code for ggplot2 and new deployment, up to line 1021

--- Algar_Cam_Traps/Algar_code_organize.R
+++ Algar_Cam_Traps/Algar_code_organize.R
@@ -991,20 +991,34 @@ xvals <- barplot(sp.plot,names.arg = NA,col="royalblue4",ylab = "Camera detectio
 text(xvals,par("usr")[3]-0.25,srt=45,adj=1.2,labels=names(sp.plot),xpd=TRUE)
 
 ## Remove humans, mustelid spp and other birds (in the most roundabout way ever...)
-no.hum <- det_data[!det_data$Species == "Homo sapiens", ]
-ani.rec <- no.hum[!no.hum$Species == "Bird spp.", ] ## Leave in identified birds
+ani.rec <- det_data[!det_data$Species == "Homo sapiens", ]
+ani.rec <- ani.rec[!ani.rec$Species == "Unknown species", ] ## Omits unknowns
+ ## Gather all birds except sandhill crane into 1 category "Bird spp."
+ani.rec$Species <- gsub("Perisoreus canadensis", "Bird spp.", ani.rec$Species)
+ani.rec$Species <- gsub("Colaptes auratus", "Bird spp.", ani.rec$Species)
+ani.rec$Species <- gsub("Tympanuchus phasianellus", "Bird spp.", ani.rec$Species)
+ani.rec$Species <- gsub("Strix nebulosa", "Bird spp.", ani.rec$Species)
+ani.rec$Species <- gsub("Canachites canadensis", "Bird spp.", ani.rec$Species)
+ani.rec$Species <- gsub("Branta canadensis", "Bird spp.", ani.rec$Species)
+
+unique(ani.rec$Species)
+
+
 
 
 sp_detect <- ani.rec$Species
 st_detect <- ani.rec$Station
 
+sp.plot1 <- rev(sort(table(sp_detect)))
+sp.plot1 <- as.data.frame(sp.plot1) ##data frame summing detections --> fix scientific names to common
+fix(sp.plot1)
+
+
 ### Frequency histograms
 
 par(mfrow = c(1,1))## Multiple plots on same page (2 rows, 1 column)
 
-sp.plot <- rev(sort(table(sp_detect))) 
-xvals <- barplot(sp.plot,names.arg = NA,col="royalblue4",ylab = "Camera detections",cex.lab=1.5,ylim=c(0,350))
-text(xvals,par("usr")[3]-0.25,srt=45,adj=1.2,labels=names(sp.plot),xpd=TRUE)
+ggplot(data = sp.plot1, aes(x = sp_detect, y = Freq)) + geom_bar(stat = "identity", fill = "lightblue", colour = "black") + theme_classic() + xlab("Species") + ylab("Total Detections") + theme(axis.text.x = element_text(angle = 45, hjust = 1, colour = "black")) + scale_x_discrete(limits = c("Sandhill crane", "Black bear", "White-tailed deer", "Grey wolf", "Snowshoe hare", "Bird spp.", "Woodland caribou", "Moose", "Coyote", "Canada lynx", "Red squirrel", "Red fox", "American marten", "River otter", "Beaver"))
 
 ###########Do same for 2015.01 deployment, i.e. removing humans and other birds#######################
 no.hum <- rec.spec[!rec.spec$Species == "H_sapiens", ]
