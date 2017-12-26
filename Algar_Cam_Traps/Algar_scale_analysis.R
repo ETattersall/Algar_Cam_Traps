53a78c0a0dc3b65345755b324fd1d8b0a8439c9a
ETattersall <ertattersall@gmail.com>
2017-12-25 16:54:21 -0800

    Starting script for scale analysis

---
+++ Algar_Cam_Traps/Algar_scale_analysis.R
@@ -0,0 +1,6 @@
+############################
+# Algar_scale_analysis.R
+# Determining which buffer sizes to use when modeling landscape variables for seismic line restoration analysis
+# Started by Erin T. Dec. 26, 2017
+# Code, advice provided by Gillian Fraser, Fisher et al., 2011
+############################
