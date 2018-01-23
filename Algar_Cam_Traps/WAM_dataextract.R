bc8d9a8c58aa6f44701e53cd76c4ba8698844b07
ETattersall <ertattersall@gmail.com>
2018-01-23 11:34:18 -0800

    Modifying MacNearney's function to clip raster data to station points as well as add a buffer surrounding points.

--- Algar_Cam_Traps/WAM_dataextract.R
+++ Algar_Cam_Traps/WAM_dataextract.R
@@ -13,11 +13,13 @@ require(sp)
 
 require(rgdal)
 
+require(rgeos)
+
 
 
 #set working directory
 
-setwd("C:/Users/ETattersall/Google Drive/Algar Seismic Restoration Project/GIS data/GIS")
+setwd("F:/WAM_extractions")
 
 
 
@@ -32,6 +34,7 @@ cams<-readOGR(dsn=getwd(), layer="AlgarSites_April2017")
 #check that projections are the same
 
 r
+
 ## class       : RasterLayer 
 ## dimensions  : 88835, 67595, 6004801825  (nrow, ncol, ncell)
 ## resolution  : 1, 1  (x, y)
@@ -62,7 +65,7 @@ plot(cams,add=T) #Stations stack onto 250m-buffered data. Station symbols extend
 
 ## Doug's code is based on a spatial lines file that needs to be rasterized and clipped. Try for Algar cam stations (spatial points)
 
-# Write a function using a simple functions
+#### Test1: Write a function using a simple functions ####
 
 # seis_SLDF is a SpatialLinesDataFrame of the seismic lines.Also works for spatial points
 
@@ -114,6 +117,7 @@ AddWAM<-function(seis_SLDF,wam_rast){
 
 
 AddWAM1<-AddWAM(cams,r) #Ran for 24 seconds
+# Test 3 (Jan 23, 9:40): Re-testing original code --> crops raster to spatial points. I need spatial polygons
 
 
 
@@ -122,7 +126,7 @@ str(AddWAM1@data) #added data to data frame
 
 head(AddWAM1@data)
 
-#### Test 2: Removing steps to crop and rasterize from function
+#### Test 2: Removing steps to crop and rasterize from function ####
 # spatial.df = spatial data frame object to add attribute data to
 # wam_rast = raster of WAM values, already clipped to desired buffer size
 
@@ -134,7 +138,7 @@ AddWAM<-function(spatial.df,wam_rast){
     
     tryCatch({ #tryCatch allows the rest of the function to run even if one line encounters an error
       
-      # clip1 <- crop(wam_rast, extent(spatial.df[i,])+1) #crop raster to extent of line segment.
+       #clip1 <- crop(wam_rast, extent(spatial.df[i,])+1) #crop raster to extent of line segment.
       
       #+1 is necessary for perfectly horizontal lines
       
@@ -142,7 +146,7 @@ AddWAM<-function(spatial.df,wam_rast){
       
       #It needs to be at least 2 wide to be considered a valid extent.
       
-      # clip2 <- rasterize(spatial.df[i,], clip1, mask=TRUE) #take the line segment and rasterize it using clip1
+      #clip2 <- rasterize(spatial.df[i,], clip1, mask=TRUE) #take the line segment and rasterize it using clip1
       
       ext <- getValues(wam_rast) #much faster than extract()
       
@@ -170,23 +174,119 @@ AddWAM<-function(spatial.df,wam_rast){
 ## Testing
 AddWAM2 <- AddWAM(spatial.df = cams, wam_rast = r) # Taking longer to run...makes sense because raster is bigger
 # ERROR: cannot allocate vector of size 2.1 GB
+# Running on lab computer --> took 40 minutes
 
 
-plot(r)
+str(AddWAM2@data) ##all means, max and min values are the same across stations (2.473, 1, 7)
+#getsValues function finds values for entire raster file
+#Current function is taking mean, min and max of entire file and inputting it to every line of the df
 
-plot(seis_test_out,add=T)
+head(AddWAM2@data)
+tail(AddWAM2@data)
 
-plot(seis_test_out[is.na(seis_test_out@data$wam_mean),],add=T, col="red")
+#### Test 4: Cropping raster to spatial polygons for buffered area within function ####
+# Loading original WAM file (not yet clipped to desired buffer)
+r2 <- raster("AlgarWAM.tif")
+
+# Function
+#spatial.df = dataframe to add WAM data to
+# wam_rast = original WAM raster layer
+# buffer = desired buffer area
+
+AddWAM<-function(spatial.df, wam_rast, buffer){
+  
+  start.time<-paste("start time:", Sys.time())
+  
+  for(i in 1:nrow(spatial.df)) {
+    
+    tryCatch({ #tryCatch allows the rest of the function to run even if one line encounters an error
+      
+      b <- gBuffer(spatial.df, width = buffer, byid=TRUE) #Creating buffer around points
+      
+      clip1 <- crop(x = wam_rast, y = extent(b[i,])) #crop raster to extent of station buffer
+      
+      ext <- getValues(clip1) #much faster than extract()
+      
+      spatial.df@data[i,paste("wam_mean", buffer, sep ="")]<- mean(ext, na.rm=T) #add 'mean' attribute to station data
+      
+      spatial.df@data[i,paste("wam_min", buffer, sep ="")]<- min(ext, na.rm=T)#add 'min' attribute to station data
+      
+      spatial.df[i,paste("wam_max", buffer, sep ="")]<- max(ext, na.rm=T)#add 'max' attribute to station data
+      
+    }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+    
+  }
+  
+  end.time<-paste("end time:", Sys.time())
+  
+  print(start.time)
+  
+  print(end.time)
+  
+  return(spatial.df)
+  
+}
+
+#Test
+AddWAM4 <- AddWAM(spatial.df = cams, wam_rast = r2, buffer = 100)
 
 
 
-diff_mean<-round(seis_out@data$WDEPTH_MEA-seis_out@data$wam_mean,0)
+plot(r2)
 
-diff_min<-round(seis_out@data$WDEPTH_MIN-seis_out@data$wam_min,0)
+plot(AddWAM4,add=T)
+
+plot(AddWAM4[is.na(AddWAM4@data$wam_mean),],add=T, col="red")
+head(AddWAM4@data)
+tail(AddWAM4@data)
+hist(AddWAM4$wam_mean) # slightly skewed normal distrib
+hist(AddWAM4$wam_max) # max is all 7, min is all 1
+
+#Try for much smaller buffer to see if it changes
+AddWAM30 <- AddWAM(spatial.df = cams, wam_rast = r2, buffer = 30)
+head(AddWAM30@data)
+hist(AddWAM30$wam_max) #mostly 7 but some lower
+
+#### TEST 4 SUCCESSFULLLLLLLLLL #####
+
+# Buffers to test: 250, 500, 1000, 2000
+WAM250 <- AddWAM(spatial.df = cams, wam_rast = r2, buffer = 250)
+WAM500 <- AddWAM(spatial.df = cams, wam_rast = r2, buffer = 500)
+WAM1000 <- AddWAM(spatial.df = cams, wam_rast = r2, buffer = 1000)
+WAM2000 <- AddWAM(spatial.df = cams, wam_rast = r2, buffer = 2000)
+
+plot(WAM250) #Spatial points data frame
+
+hist(WAM250$wam_mean250)
+hist(WAM500$wam_mean500)
+hist(WAM1000$wam_mean1000)
+hist(WAM2000$wam_mean2000)
+# Means become more normal, mins are all 1 and maxs are all 7
+# Makes sense that data becomes more normal as larger areas are averaged
+
+WAMmeans <- cbind(WAM250$wam_mean250, WAM500$wam_mean500, WAM1000$wam_mean1000, WAM2000$wam_mean2000)
+colnames(WAMmeans) <- c("WAM250", "WAM500", "wAM1000", "WAM2000")
+str(WAMmeans)
+head(WAMmeans)
+summary(WAMmeans) #Means, maxes, and mins are very similar. 
+                  #When ranges are converted to wetness index categories they will be the same.
+
+write.csv(WAMmeans, "WAMmeans_colourRanges.csv")
+
+#Saving WAM500m and WAM250m
+writeOGR(obj=WAM250, dsn=getwd(), layer="WAM_250mbuffer", driver="ESRI Shapefile")
+writeOGR(obj=WAM500, dsn=getwd(), layer="WAM_500mbuffer", driver="ESRI Shapefile")
 
 
 
-hist(diff_mean)
+diff_mean<-round(AddWAM2@data$WDEPTH_MEA-AddWAM2@data$wam_mean,0)
+
+diff_min<-round(AddWAM2@data$WDEPTH_MIN-AddWAM2@data$wam_min,0)
+
+
+
+
+
 
 hist(diff_min)
 
@@ -198,7 +298,7 @@ table(diff_min)
 
 
 
-#Third attempt: using parallel processing to speed things up:
+#### Doug's Third attempt: using parallel processing to speed things up: ####
 
 #initiate multicore cluster and load packages
 
