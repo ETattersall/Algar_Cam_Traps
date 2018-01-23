1711ba862ddd04979fd0ae7ef87f35e8ad7b89f4
ETattersall <ertattersall@gmail.com>
2018-01-23 14:12:39 -0800

    Checking survey effort

--- Algar_Cam_Traps/Camelot_rtDataextract.R
+++ Algar_Cam_Traps/Camelot_rtDataextract.R
@@ -8,6 +8,7 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(camtrapR)
 
 ## Import recordTables created by Camelot
 setwd("C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Data")
@@ -73,4 +74,55 @@ sp.plot1 <- rev(sort(table(sp.1)))
 sp.plot1 <- as.data.frame(sp.plot1) ##data frame summing detections --> fix scientific names to common
 fix(sp.plot1)
 ggplot(data = sp.plot1, aes(x = sp.1, y = Freq)) + geom_bar(stat = "identity", fill = "lightblue", colour = "black") + theme_classic() + xlab("Species") + ylab("Total Detections") + theme(axis.text.x = element_text(angle = 45, hjust = 1, colour = "black")) + scale_x_discrete(limits = c("Sandhill crane", "Black bear", "White-tailed deer", "Grey wolf", "Snowshoe hare", "Bird spp.", "Woodland caribou",  "Moose", "Coyote",  "Canada lynx", "Red squirrel", "Red fox", "American marten", "River otter", "Beaver"))
+
+##### Checking Survey effort with camtrapR ######
+camdata <- read.csv("AlgarStations_DeploymentData.csv")
+
+## plotting cam stations
+with(camdata,plot(utmE,utmN))
+
+
+
+# specify folder with renamed images
+setwd(images_wd)
+
+
+# check number of JPG images [this stuff isn't needed now that we have tagged images]
+length(list.files(images_wd, pattern = "JPG", recursive = TRUE)) # [1] 63840 --> includes stations with misfiring cameras
+
+# try this for each station (Image count per Station)
+stations <- as.character(camdata$CamStation[1:60]) ## Need to exclude offline sites, which currently have no data
+
+
+stationDir <- paste("E:/Algar_Apr-Nov2017/Renamed_Images",stations,sep="/")
+
+stn.img <- rep(NA,length(stations))
+for (i in 1:length(stations)) {
+  stn.img[i] <- length(list.files(stationDir[i], pattern = "JPG"))
+}
+sum(stn.img)
+
+## Create a station summary data frame for this deployment
+stn.sum <- camdata[1:60,] %>% select(CamStation, utmE, utmN, Treatment, Session3Start, Problem2_from, Problem2_to,Session4Start)
+## Ad station images to stn.sum
+stn.sum$StationImages <- stn.img
+
+# tally survey effort
+
+
+## Added columns "Problem1_from" and "Problem1_to" to CTtable ('Algar_stationdata.2016.01.csv')
+
+camEff <- cameraOperation(stn.sum, 
+                          stationCol = "CamStation", 
+                          setupCol = "Session3Start", 
+                          retrievalCol = "Session4Start",
+                          hasProblems = TRUE,
+                          dateFormat = "%d/%m/%Y", 
+                          writecsv = FALSE)
+
+sum(camEff,na.rm=T) ## 7703
+
+# days per station
+summary(rowSums(camEff,na.rm=T)) # mean = 128.4, median = 203, range = 0 - 206
+
 
