dc4a98957c04f6d1553341a910950085f12a3bcd
ETattersall <ertattersall@gmail.com>
2018-02-05 17:55:21 -0800

    Including a random effect of month in all models, plotting results

--- Algar_Cam_Traps/Algar_bear_glmms.R
+++ Algar_Cam_Traps/Algar_bear_glmms.R
@@ -102,5 +102,66 @@ lrtest(b2.NB, b1.NB)
 
 summary(b2.NB)
 
+# Including a random effect of Yr_Month
+b5.NB <- glmmTMB(Blackbear~Treatment +low500 + (1|Site) + (1|Yr_Month), data = bear, family = nbinom2)
+
+
+
+AICctab(b0.NB, b1.NB, b2.NB, b5.NB)
+#     dAICc df
+# b5.NB  0.0  8 
+# b2.NB 19.1  7 
+# b1.NB 29.8  6 
+# b0.NB 36.8  3
+
+lrtest(b5.NB, b2.NB)
+# Model 1: Blackbear ~ Treatment + low500 + (1 | Site) + (1 | Yr_Month)
+# Model 2: Blackbear ~ Treatment + low500 + (1 | Site)
+#    #Df  LogLik Df  Chisq Pr(>Chisq)    
+# 1   8 -203.23                         
+# 2   7 -213.87 -1 21.284   3.96e-06 ***
+
+summary(b2.NB)
+summary(b5.NB) #Yr_Month accounts for more variance than site
+
+#Include (1|Yr_Month) for all models
+# Model 0: Null
+b0.NB <- glmmTMB(Blackbear~1 + (1|Site)+ (1|Yr_Month), data = bear, family = nbinom2)
+# Model 1. Blackbear as a function of treatment
+b1.NB <- glmmTMB(Blackbear~Treatment + (1|Site) + (1|Yr_Month), data = bear, family = nbinom2)
+
+# Model 2: Blackbear as a function of treatment and %Low
+b2.NB <- glmmTMB(Blackbear~Treatment +low500 + (1|Site)+ (1|Yr_Month), data = bear, family = nbinom2)
+
+# Model 3: Blackbear as a function of treatment, low, and interaction
+b3.NB <- glmmTMB(Blackbear~Treatment*low500 + (1|Site)+ (1|Yr_Month), data = bear, family = nbinom2) #convergence issue still. Exclude from analysis
+
+b4.NB <- glmmTMB(Blackbear~low500 + (1|Site)+ (1|Yr_Month), data = bear, family = nbinom2)
+
+modnames <- c("Null", "Treat", "Treat + Low500", "Low500")
+beartab <- ICtab(b0.NB, b1.NB, b2.NB,b4.NB, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
+beartab
+#             dLogLik dAIC df weight
+# Treat + Low500 6.9     0.0  8  0.644 
+# Low500         3.2     1.5  5  0.308 
+# Null           0.0     5.8  4  0.035 
+# Treat          2.0     7.8  7  0.013 
+
+lrtest(b2.NB, b4.NB)
+# Model 1: Blackbear ~ Treatment + low500 + (1 | Site) + (1 | Yr_Month)
+# Model 2: Blackbear ~ low500 + (1 | Site) + (1 | Yr_Month)
+#   #Df  LogLik Df  Chisq Pr(>Chisq)  
+# 1   8 -203.23                       
+# 2   5 -206.97 -3 7.4729    0.05826 .
+
+summary(b2.NB)
+
 # Comparing %lowland and bear detections
 plot(x = bear$low500, y = bear$Blackbear) #Pretty clear that detections decrease with ince in %lowland
+
+
+bbear <- ggplot(data = bear, aes(x = Treatment, y = Blackbear, fill = Treatment)) + geom_boxplot()
+bbear + theme_classic() + xlab("Sampling Strata") + ylab("Black bear detections/month") + scale_x_discrete(limits=c("HumanUse", "Control", "SPP", "NatRegen")) + scale_fill_manual(values=c("orange", "red",  "light green", "purple" )) +theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 0, colour = "black", size = 12)) + theme(axis.title.x = element_text(angle = 0, colour = "black", size = 14)) + theme(axis.title.y = element_text(angle = 90, colour = "black", size = 14))
+
+bear.low <- ggplot(data = bear, aes(x = low500, y = Blackbear)) + geom_point() + theme_classic() + xlab("Proportion lowland habitat (500m buffer") + ylab("Black bear detections/month")
+bear.low
