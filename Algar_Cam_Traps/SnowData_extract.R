802858b7f5c569e00dfc253fa25e60fa9c4fcc7b
ETattersall <ertattersall@gmail.com>
2018-03-14 13:53:06 -0700

    Updated Snow data extraction for Apr-Nov2017 data, including adding NA rows for inactive months for all failed cameras

--- Algar_Cam_Traps/SnowData_extract.R
+++ Algar_Cam_Traps/SnowData_extract.R
@@ -37,21 +37,21 @@ all <- bind_rows(csv.list) # Generates error: column (x) can't be converted from
 all <- csv.list %>%
   lapply(function(x) mutate_each(x, funs('as.character'))) %>%
   bind_rows() %>% 
-  mutate_each(funs('as.factor')) # Worked with error `mutate_each()` is deprecated. Use `mutate_all()`, `mutate_at()` or `mutate_if()` instead.
+  mutate_all(funs('as.factor')) # Worked with error `mutate_each()` is deprecated. Use `mutate_all()`, `mutate_at()` or `mutate_if()` instead.
 
-str(all) #56 levels for folders: 2 were inactive, Algar49 and Algar51 not done. Emailed Nisha, set aside for now (Jan. 30)
+str(all) #57 levels for folders: addressed above
 
 Snow <- all$Snow
 Snow
 Snow <- toupper(Snow) #All characters to upper case
 unique(Snow)
-table(Snow) #1592 FALSE, 7460 TRUE
+table(Snow) #6958 FALSE, 665 TRUE
 
 
 #Convert to numeric
 Snow[which(Snow==TRUE)] <- 1
 Snow[which(Snow==FALSE)] <- 0
-table(Snow) #1592 FALSE, 7460 TRUE
+table(Snow) #6958 = 0, 665 = 1
 
 # Create data.frame of station, date, Snow data
 # Station == all$Folder
@@ -163,15 +163,15 @@ maxdate[,2] - mindate[,2] # (Needs to be ordered by Station here)
 
 S$DateStart <- min(mindate[,2]) #Adding the date of first detection to S dataframe
 S$DateStart <- (strptime(S$DateStart, "%Y-%m-%d", tz="MST")) ##Adding a column for the first day of the study
-DateStart <- min(S$DateStart) # The first detection from all stations - 2016-11-11 - first detection of the deployment
+DateStart <- min(S$DateStart) # The first detection from all stations - 2017-04-19 - first detection of the deployment
 str(S)
 
 # calculate a unique day for each day of study
 # since there is no start time entered, this should work from hour 0, so should be same as calendar day
 # using "floor" so it doesn't round up to next day
 S$StudyDay <- floor(as.numeric(difftime(S$Date,min(S$DateStart),units="days"))) ##Takes difference between the detection date and start date, without rounding up
-S$StudyDay <- S$StudyDay+1 #Turns start date into day 1, not day 0
-summary(S$StudyDay) # 161 study days
+S$StudyDay <- S$StudyDay+2 #Turns start date into day 1, not day -1
+summary(S$StudyDay) # 205 study days
 
 
 #### Aggregating snow data into average per month (again, adapting Jo's code) ####
@@ -185,7 +185,7 @@ summary(S) #snow is character, convert to numeric
 S$Snow <- as.numeric(S$Snow)
 summary(S)
 levels(S$Yr_month)
-sum(S$Snow) #7460 snow days total
+sum(S$Snow) #665 snow days total
 
 #Subsetting for Station, StudyDay, Snow, Yr_Month columns (group_by has issue with POSIXct format from S)
 S1 <- as.data.frame(S$Station)
@@ -199,17 +199,169 @@ m.snow <- S1 %>%
   group_by(Station, Yr_month) %>% 
   summarise(sum(Snow, na.rm = TRUE))
 colnames(m.snow) <- c("Station","Yr_Month","Snow")
-summary(m.snow) #max 31 snow days in a month, min 0, mean 21.8, median 28
-sum(m.snow$Snow) #7460 snow days in deployment
+summary(m.snow) #max 13 snow days in a month, min 0, mean 2.065, median 0
+sum(m.snow$Snow) #665 snow days in deployment
 
 plot(x = m.snow$Yr_Month, y = m.snow$Snow, xlab = "Year-Month",ylab = "Snow Days")
 
 #### Aggregate with detection data ####
 setwd("C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Data")
-#Load in monthly detection data for 2nd deployment (Nov. 2016-Apr 2017)
-dep1 <- read.csv("2016.01_monthlydetections.csv") #330 observations, as opposed to 342 in m.snow
+#Load in monthly detection data for 3rd deployment (Apr-Nov 2017)
+dep1 <- read.csv("2017.01_monthlydetections.csv") #472 observations, as opposed to 322 in m.snow
 dep1$X <- NULL
-#m.snow has 6 months of data for 52 and 56, which do not have detections
+##m.snow does not include NA months for failed cameras
+
+## Add in 8 NA months for cameras with no snow data: Algar06, Algar21, Algar43
+#Station column
+Algar06 <- rep("Algar06",8)
+Algar21 <- rep("Algar21",8)
+Algar43 <- rep("Algar43",8)
+Station <- append(x = Algar06, values = c(Algar21, Algar43))
+
+#Yr_Month column
+Yr_Month <- rep(c("2017-04", "2017-05", "2017-06", "2017-07", "2017-08", "2017-09", "2017-10", "2017-11"),3)
+#Snow column
+Snow <- rep(NA,24)
+
+# Combine into data frame of NA snow months
+NAmonths <- cbind.data.frame(Station,Yr_Month, Snow)
+
+## Add NA months to m.snow
+m.snow <- rbind.data.frame(NAmonths,m.snow)
+m.snow <- with(m.snow, m.snow[order(as.factor(as.character(Station)), Yr_Month), ])
+
+## Remove Algar32: 2017-04,  (no data, only active for 3 days)
+m.snow[192,] #confirming row number (row num. in data frame does not match)
+m.snow <- m.snow[-192, ]
+
+## If snow data was collected in partial months --> keep if active days > 15 days (same rules as detections)
+# Partial months during which snow data was collected (need to be switched to NAs):
+# Algar11: 2017-06, Algar12:2017-06, Algar18: 2017-04, Algar19: 2017-06, Algar23: 2017-06, Algar35: 2017-06, 
+# Algar40: 2017-07, Algar42: 2017-06, Algar44: 2017-06, Algar48:2017-06
+#Algar27: active full time, but tree fell in Sep. Change 2017-10 and 2017-11 to NA
+# Change manually
+fix(m.snow)
+
+## 1. Add NA months for failed cameras May - Nov (7 months): Algar07, Algar13, Algar14, Algar51
+Algar07 <- rep("Algar07", 7)
+Algar13 <- rep("Algar13", 7)
+Algar14 <- rep("Algar14", 7)
+Algar51 <- rep("Algar51", 7)
+Station <- append(x = Algar07, values = c(Algar13, Algar14, Algar51))
+#Yr_Month column
+Yr_Month <- rep(c("2017-05", "2017-06", "2017-07", "2017-08", "2017-09", "2017-10", "2017-11"),4)
+#Snow column
+Snow <- rep(NA,28)
+
+# Combine into data frame of NA snow months
+NAmonths <- cbind.data.frame(Station,Yr_Month, Snow)
+
+## Add NA months to m.snow
+m.snow <- rbind.data.frame(NAmonths,m.snow)
+m.snow <- with(m.snow, m.snow[order(as.factor(as.character(Station)), Yr_Month), ])
+
+##2. Add NA months for failed cameras Jun - Nov (6 months): Algar09, Algar18, Algar52, Algar55, Algar60
+Algar09 <- rep("Algar09", 6)
+Algar18 <- rep("Algar18", 6)
+Algar52 <- rep("Algar52", 6)
+Algar55 <- rep("Algar55", 6)
+Algar60 <- rep("Algar60", 6)
+
+Station <- append(x = Algar09, values = c(Algar18, Algar52, Algar55, Algar60))
+#Yr_Month column
+Yr_Month <- rep(c("2017-06", "2017-07", "2017-08", "2017-09", "2017-10", "2017-11"),5)
+#Snow column
+Snow <- rep(NA,30)
+
+# Combine into data frame of NA snow months
+NAmonths <- cbind.data.frame(Station,Yr_Month, Snow)
+
+## Add NA months to m.snow
+m.snow <- rbind.data.frame(NAmonths,m.snow)
+m.snow <- with(m.snow, m.snow[order(as.factor(as.character(Station)), Yr_Month), ])
+
+##3. Add NA months for failed cameras Jul - Nov (5 months): Algar11, Algar12, Algar19, Algar23, Algar35, Algar42, Algar44, Algar48, Algar57, Algar59
+Algar11 <- rep("Algar11", 5)
+Algar12 <- rep("Algar12", 5)
+Algar19 <- rep("Algar19", 5)
+Algar23 <- rep("Algar23", 5)
+Algar35 <- rep("Algar35", 5)
+Algar42 <- rep("Algar42", 5)
+Algar44 <- rep("Algar44", 5)
+Algar48 <- rep("Algar48", 5)
+Algar57 <- rep("Algar57", 5)
+Algar59 <- rep("Algar59", 5)
+
+Station <- append(x = Algar11, values = c(Algar12, Algar19, Algar23, Algar35, Algar42, Algar44, Algar48, Algar57, Algar59))
+#Yr_Month column
+Yr_Month <- rep(c("2017-07", "2017-08", "2017-09", "2017-10", "2017-11"),10)
+#Snow column
+Snow <- rep(NA,50)
+
+# Combine into data frame of NA snow months
+NAmonths <- cbind.data.frame(Station,Yr_Month, Snow)
+
+## Add NA months to m.snow
+m.snow <- rbind.data.frame(NAmonths,m.snow)
+m.snow <- with(m.snow, m.snow[order(as.factor(as.character(Station)), Yr_Month), ])
+
+##4. Add NA months for failed cameras Aug - Nov. (4 months): Algar40, Algar58
+Algar40 <- rep("Algar40", 4)
+Algar58 <- rep("Algar58", 4)
+
+Station <- append(x = Algar40, values = Algar58)
+#Yr_Month column
+Yr_Month <- rep(c("2017-08", "2017-09", "2017-10", "2017-11"), 2)
+#Snow column
+Snow <- rep(NA,8)
+
+# Combine into data frame of NA snow months
+NAmonths <- cbind.data.frame(Station,Yr_Month, Snow)
+
+## Add NA months to m.snow
+m.snow <- rbind.data.frame(NAmonths,m.snow)
+m.snow <- with(m.snow, m.snow[order(as.factor(as.character(Station)), Yr_Month), ])
+
+##5. Algar30 (inactive Jul-Nov 2017 - 5 months --> missed before)
+Station <- rep("Algar30", 5)
+#Yr_Month column
+Yr_Month <- c("2017-07", "2017-08", "2017-09", "2017-10", "2017-11")
+#Snow column
+Snow <- rep(NA,5)
+
+# Combine into data frame of NA snow months
+NAmonths <- cbind.data.frame(Station,Yr_Month, Snow)
+
+## Add NA months to m.snow
+m.snow <- rbind.data.frame(NAmonths,m.snow)
+m.snow <- with(m.snow, m.snow[order(as.factor(as.character(Station)), Yr_Month), ])
+
+## Still a 6 row discrepancy...
+## Also ordered incorrectly... shouldn't be a problem in long run but difficult to see missing data
+str(m.snow)
+table(m.snow$Station) # Algar30 has 9, Algar41 has 1
+# Algar30 2017-07 is repeated, with one row Snow = NA, other Snow = 0 --> remove 0
+m.snow[234, ] # Found with trial and error :P
+m.snow <- m.snow[-234, ]
+
+##Algar 41 missed: need NAs for May-Nov
+Station <- rep("Algar41", 7)
+#Yr_Month column
+Yr_Month <- c("2017-05", "2017-06","2017-07", "2017-08", "2017-09", "2017-10", "2017-11")
+#Snow column
+Snow <- rep(NA,7)
+
+# Combine into data frame of NA snow months
+NAmonths <- cbind.data.frame(Station,Yr_Month, Snow)
+
+## Add NA months to m.snow
+m.snow <- rbind.data.frame(NAmonths,m.snow)
+m.snow <- with(m.snow, m.snow[order(as.factor(as.character(Station)), Yr_Month), ])
+
+
+
+################
+#(2nd deployment)m.snow has 6 months of data for 52 and 56, which do not have detections
 ## Empty detection rows need to be added to dep1 for 52 and 56 (copy code from Algar_combine_datasets.R)
 ##Dataframe of 12x11
 # Site column
@@ -241,12 +393,13 @@ tail(dep1)
 
 
 
-#Add unique identifier Site_ym to snow dataframe
+#### Combining SnowDays data to detection data ####
+## Add unique identifier Site_ym to snow dataframe ####
 m.snow$Site_ym <- paste(m.snow$Station, m.snow$Yr_Month)
 
 dep1$SnowDays <- m.snow$Snow[match(dep1$Site_ym, m.snow$Site_ym)]
 
-write.csv(dep1, "2016.01_monthlydetections.csv")
+write.csv(dep1, "2017.01_monthlydetections.csv")
 
 plot(x = dep1$SnowDays, y = dep1$Blackbear) # Highest detections in months with less snow - unsurprising
 plot(x = dep1$SnowDays, y = dep1$Wolf) # No clear trends
