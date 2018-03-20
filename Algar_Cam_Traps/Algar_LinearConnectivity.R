261ba4b5e6f8e3355aa62d86dbe95b27e185f235
ETattersall <ertattersall@gmail.com>
2018-03-20 16:52:03 -0700

    Reclipping HF from original ABMI layer, calculating seismic line length

--- Algar_Cam_Traps/Algar_LinearConnectivity.R
+++ Algar_Cam_Traps/Algar_LinearConnectivity.R
@@ -27,21 +27,26 @@ summary(AlgarHF$PUBLIC_COD) # Linear disturbances = Seismic lines (by far the mo
 #3. Distance to intersections
 #### All need to be measured at a few scales
 
+
+#### Take 2: Re-clipping ABMI HF layer to desired scales ####
+ABMI_HF <- readOGR("GIS", "CameraArray_ABMI_HFI_2014")
+plot(ABMI_HF) ## HF for Christina Lakes, Algar, and Richardson
+summary(ABMI_HF)
+
+## Convert to UTM
+HF_UTM <- spTransform(ABMI_HF, CRSobj = CRS(proj4string(Algcoord)))
+proj4string(HF_UTM)
+ABMI_HF <- HF_UTM #Overwrite original HF CRS
+summary(ABMI_HF)
+plot(ABMI_HF)
+
 ## Step 1: Subset HF layer for linear disturbances only
 ## Only interested in area <2km around cam stations at most. Most prevalent linear disturbances will be seismic line, with a few pipelines (1-2 cams are on pipelines)
 ## Subset HF for seismic and pipe lines in Algar
-Lines <- AlgarHF[AlgarHF$PUBLIC_COD=="Seismic line" | AlgarHF$PUBLIC_COD=="Pipeline" , ]
-summary(Lines) ## UTM. Also includes cam station data, which is not necessary (will be added by clip with Algcoord)
-str(Lines@data) #keep columns 1:3 only (just need linear attribute data)
-Linesdata <- Lines@data
-Lines <- Lines[,1:3]
-Linesdata <- Lines@data
-summary(Lines) #retain spatial and line attributes
-summary(Linesdata)
-hist(Linesdata$Length_m)
+Lines <- ABMI_HF[ABMI_HF$PUBLIC_COD=="Seismic line" | ABMI_HF$PUBLIC_COD=="Pipeline" , ]
+summary(Lines)
 
-## Step 2: Create buffers around cam sites and extract line data within buffers (from Algar_scale_analysis.R)
-
+## Clip ABMI_HF to 8 scales around Algar cameras only
 # Creating the 8 buffers to be used (250-2000m)
 b250 <- gBuffer(Algcoord, width = 250, byid = TRUE)
 b500 <- gBuffer(Algcoord, width = 500, byid = TRUE)
@@ -56,75 +61,56 @@ b2000 <- gBuffer(Algcoord, width = 2000, byid = TRUE)
 int250 <- raster::intersect(Lines, b250)
 plot(int250) #worked correctly
 summary(int250) #attribute data retained
+data250 <- int250@data #131 lines, each unique (checked with unique(data250))
+
+
+## Length is total length of line segment, not length within buffer. Verify by calculating length and comparing
+int250$Length250<- gLength(int250, byid = TRUE)
+data250 <- int250@data
+#Order by Station and Length
+data250 <- data250[with(data250, order(CamStation, Length_m)), ] ### Calculated lengths are not = to original lengths, but seem too long. Compare as buffer size increases
+
 
 int500 <- raster::intersect(Lines, b500)
-int750 <- raster::intersect(Lines, b750)
-int1000 <- raster::intersect(Lines, b1000)
-int1250 <- raster::intersect(Lines, b1250)
-int1500 <- raster::intersect(Lines, b1500)
-int1750 <- raster::intersect(Lines, b1750)
-int2000 <- raster::intersect(Lines, b2000)
-
-data250 <- int250@data
-
+int500$Length500<- gLength(int500, byid = TRUE)
 data500 <- int500@data
-## Length_m = total line's length. But may not accurately depict functional linear length, as some seismic lines are linked to pipelines and other seismic lines
-## Trying to figure out what each row is in int250, int500, etc
-table(data250$Length_m) 
-table(data250$CamStation)
-## Order data250 by Station
-data250 <- data250[with(data250, order(CamStation, Length_m)), ]
-## Many rows are duplicated. 
-data250 <- unique(data250) #Remove duplicates
-data250 <- data250[with(data250, order(CamStation, Length_m)), ] #Now number of lines at each station matches what is shown when layer is plotted (roughly; some lines that appear continuous may actually be 2 separate lines)
-table(data250$CamStation)
-
-## Removing duplicates in dataframes for all
-plot(int500)
-data500 <- int500@data
-data500 <- unique(data500) #Remove duplicates
+#Order by Station and Length
 data500 <- data500[with(data500, order(CamStation, Length_m)), ]
-table(data500$CamStation)
 
+
+int750 <- raster::intersect(Lines, b750)
+int750$Length750<- gLength(int750, byid = TRUE)
 data750 <- int750@data
-data750 <- unique(data750) #Remove duplicates
+#Order by Station and Length
 data750 <- data750[with(data750, order(CamStation, Length_m)), ]
 
+int1000 <- raster::intersect(Lines, b1000)
+int1000$Length1000<- gLength(int1000, byid = TRUE)
 data1000 <- int1000@data
-data1000 <- unique(data1000) #Remove duplicates
+#Order by Station and Length
 data1000 <- data1000[with(data1000, order(CamStation, Length_m)), ]
-table(data1000$CamStation)
 
+int1250 <- raster::intersect(Lines, b1250)
+int1250$Length1250<- gLength(int1250, byid = TRUE)
 data1250 <- int1250@data
-data1250 <- unique(data1250) #Remove duplicates
+#Order by Station and Length
 data1250 <- data1250[with(data1250, order(CamStation, Length_m)), ]
-table(data1250$CamStation)
 
+int1500 <- raster::intersect(Lines, b1500)
+int1500$Length1500<- gLength(int1500, byid = TRUE)
 data1500 <- int1500@data
-data1500 <- unique(data1500) #Remove duplicates
+#Order by Station and Length
 data1500 <- data1500[with(data1500, order(CamStation, Length_m)), ]
-table(data1500$CamStation)
 
+int1750 <- raster::intersect(Lines, b1750)
+int1750$Length1750<- gLength(int1750, byid = TRUE)
 data1750 <- int1750@data
-data1750 <- unique(data1750) #Remove duplicates
+#Order by Station and Length
 data1750 <- data1750[with(data1750, order(CamStation, Length_m)), ]
-table(data1750$CamStation)
 
+
+int2000 <- raster::intersect(Lines, b2000)
+int2000$Length2000<- gLength(int2000, byid = TRUE)
 data2000 <- int2000@data
-data2000 <- unique(data2000) #Remove duplicates
-data2000 <- data2000[with(data2000, order(CamStation, Length_m)), ]
-table(data2000$CamStation)
-
-## BUT calculating length
-
-##3. Calculate line density
-#a. is calculated length the same as length_m?
-int250$Length250<- gLength(int250, byid = TRUE)
-
-summary(data250)
-
-int500$Length500 <- gLength(int500, byid = TRUE)
-data500 <- int500@data
-data500 <- unique(data500) #Remove duplicates
-data500 <- data500[with(data500, order(CamStation, Length_m)), ]
-summary(data500)
+#Order by Station and Length
+data2000 <- data2000[with(data2000, order(CamStation, Length_m)), ] ## Number of lines per site increases, total length increases
