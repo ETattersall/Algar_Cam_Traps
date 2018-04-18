d53dbcc7b866848b16440add4ae94afcbc8a825c
ETattersall <ertattersall@gmail.com>
2018-04-18 13:55:03 -0700

    GLMs and model selection for choosing scales to measure lowland habitat

---
+++ Algar_Cam_Traps/Lowland_ScaleAnalysis.R
@@ -0,0 +1,165 @@
+#######################################
+# Lowland_scaleAnalysis.R
+# Running GLMs to determine appropriate scale for lowland data
+# Started Apr.18, 2018 by Erin
+######################################
+
+library(dplyr)
+require(glmmTMB)
+require(bbmle)
+
+setwd("C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Data")
+
+#Load detection data
+det <- read.csv("MonthlyDetections_nov2015-nov2017.csv")
+#Load lowland data
+low <- read.csv("newAVIE_lowland8buffersizes.csv")
+low$X <- NULL
+
+## Combine datasets
+det$low250 <- low$low250[match(det$Site, low$CamStation)]
+det$low500 <- low$low500[match(det$Site, low$CamStation)]
+det$low750 <- low$low750[match(det$Site, low$CamStation)]
+det$low1000 <- low$low1000[match(det$Site, low$CamStation)]
+det$low1250 <- low$low1250[match(det$Site, low$CamStation)]
+det$low1500 <- low$low1500[match(det$Site, low$CamStation)]
+det$low1750 <- low$low1750[match(det$Site, low$CamStation)]
+det$low2000 <- low$low2000[match(det$Site, low$CamStation)]
+
+### Wolf models
+Wolf.0 <- glmmTMB(Wolf~1, data = det, family = nbinom2)
+Wolf.250 <- glmmTMB(Wolf~low250, data = det, family = nbinom2)
+Wolf.500 <- glmmTMB(Wolf~low500, data = det, family = nbinom2)
+Wolf.750 <- glmmTMB(Wolf~low750, data = det, family = nbinom2)
+Wolf.1000 <- glmmTMB(Wolf~low1000, data = det, family = nbinom2)
+Wolf.1250 <- glmmTMB(Wolf~low1250, data = det, family = nbinom2)
+Wolf.1500 <- glmmTMB(Wolf~low1500, data = det, family = nbinom2)
+Wolf.1750 <- glmmTMB(Wolf~low1750, data = det, family = nbinom2)
+Wolf.2000 <- glmmTMB(Wolf~low2000, data = det, family = nbinom2)
+
+## Model selection with AICctab (bbmle) --> exluding VegHt
+modnames <- c("NULL","250m", "500m", "750m", "1000m", "1250m", "1500", "1750", "2000")
+wolftab <- ICtab(Wolf.0,Wolf.250,Wolf.500,Wolf.750,Wolf.1000,Wolf.1250, Wolf.1500, Wolf.1750, Wolf.2000, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
+wolftab
+
+#    dLogLik dAIC df weight
+# 500m  4.6     0.0  3  0.7125
+# 2000  2.7     3.9  3  0.1033
+# 750m  2.3     4.6  3  0.0703
+# 1000m 1.8     5.7  3  0.0412
+# 1750  1.2     6.9  3  0.0223
+# NULL  0.0     7.3  2  0.0190
+# 1500  0.6     8.1  3  0.0122
+# 1250m 0.5     8.2  3  0.0116
+# 250m  0.1     9.0  3  0.0077
+
+### Caribou models
+Caribou.0 <- glmmTMB(Caribou~1, data = det, family = nbinom2)
+Caribou.250 <- glmmTMB(Caribou~low250, data = det, family = nbinom2)
+Caribou.500 <- glmmTMB(Caribou~low500, data = det, family = nbinom2)
+Caribou.750 <- glmmTMB(Caribou~low750, data = det, family = nbinom2)
+Caribou.1000 <- glmmTMB(Caribou~low1000, data = det, family = nbinom2)
+Caribou.1250 <- glmmTMB(Caribou~low1250, data = det, family = nbinom2)
+Caribou.1500 <- glmmTMB(Caribou~low1500, data = det, family = nbinom2)
+Caribou.1750 <- glmmTMB(Caribou~low1750, data = det, family = nbinom2)
+Caribou.2000 <- glmmTMB(Caribou~low2000, data = det, family = nbinom2)
+
+## Model selection with AICctab (bbmle) --> exluding VegHt
+modnames <- c("NULL","250m", "500m", "750m", "1000m", "1250m", "1500", "1750", "2000")
+Cariboutab <- ICtab(Caribou.0,Caribou.250,Caribou.500,Caribou.750,Caribou.1000,Caribou.1250, Caribou.1500, Caribou.1750, Caribou.2000, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
+Cariboutab
+
+#    dLogLik dAIC df weight
+# 1750  30.9     0.0 3  0.758 
+# 2000  29.2     3.3 3  0.144 
+# 1500  28.7     4.3 3  0.086 
+# 1250m 26.6     8.5 3  0.011 
+# 1000m 22.2    17.3 3  <0.001
+# 750m  18.0    25.7 3  <0.001
+# 500m  12.4    37.0 3  <0.001
+# 250m   3.1    55.5 3  <0.001
+# NULL   0.0    59.8 2  <0.001
+
+### WTDeer models
+WTDeer.0 <- glmmTMB(WTDeer~1, data = det, family = nbinom2)
+WTDeer.250 <- glmmTMB(WTDeer~low250, data = det, family = nbinom2)
+WTDeer.500 <- glmmTMB(WTDeer~low500, data = det, family = nbinom2)
+WTDeer.750 <- glmmTMB(WTDeer~low750, data = det, family = nbinom2)
+WTDeer.1000 <- glmmTMB(WTDeer~low1000, data = det, family = nbinom2)
+WTDeer.1250 <- glmmTMB(WTDeer~low1250, data = det, family = nbinom2)
+WTDeer.1500 <- glmmTMB(WTDeer~low1500, data = det, family = nbinom2)
+WTDeer.1750 <- glmmTMB(WTDeer~low1750, data = det, family = nbinom2)
+WTDeer.2000 <- glmmTMB(WTDeer~low2000, data = det, family = nbinom2)
+
+## Model selection with AICctab (bbmle) --> exluding VegHt
+modnames <- c("NULL","250m", "500m", "750m", "1000m", "1250m", "1500", "1750", "2000")
+WTDeertab <- ICtab(WTDeer.0,WTDeer.250,WTDeer.500,WTDeer.750,WTDeer.1000,WTDeer.1250, WTDeer.1500, WTDeer.1750, WTDeer.2000, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
+WTDeertab
+
+#    dLogLik dAIC df weight
+# 2000  41.3     0.0 3  1     
+# 1750  33.6    15.3 3  <0.001
+# 1500  25.8    30.9 3  <0.001
+# 500m  21.1    40.4 3  <0.001
+# 1250m 21.0    40.6 3  <0.001
+# 1000m 20.4    41.9 3  <0.001
+# 250m  20.0    42.6 3  <0.001
+# 750m  18.4    45.8 3  <0.001
+# NULL   0.0    80.6 2  <0.001
+
+### Moose models
+Moose.0 <- glmmTMB(Moose~1, data = det, family = nbinom2)
+Moose.250 <- glmmTMB(Moose~low250, data = det, family = nbinom2)
+Moose.500 <- glmmTMB(Moose~low500, data = det, family = nbinom2)
+Moose.750 <- glmmTMB(Moose~low750, data = det, family = nbinom2)
+Moose.1000 <- glmmTMB(Moose~low1000, data = det, family = nbinom2)
+Moose.1250 <- glmmTMB(Moose~low1250, data = det, family = nbinom2)
+Moose.1500 <- glmmTMB(Moose~low1500, data = det, family = nbinom2)
+Moose.1750 <- glmmTMB(Moose~low1750, data = det, family = nbinom2)
+Moose.2000 <- glmmTMB(Moose~low2000, data = det, family = nbinom2)
+
+## Model selection with AICctab (bbmle) --> exluding VegHt
+modnames <- c("NULL","250m", "500m", "750m", "1000m", "1250m", "1500", "1750", "2000")
+Moosetab <- ICtab(Moose.0,Moose.250,Moose.500,Moose.750,Moose.1000,Moose.1250, Moose.1500, Moose.1750, Moose.2000, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
+Moosetab
+
+#    dLogLik dAIC df weight
+# 250m  2.4     0.0  3  0.438 
+# 500m  1.4     2.0  3  0.164 
+# NULL  0.0     2.8  2  0.110 
+# 750m  0.5     3.8  3  0.067 
+# 1000m 0.2     4.3  3  0.051 
+# 1250m 0.1     4.5  3  0.046 
+# 1500  0.1     4.6  3  0.043 
+# 1750  0.0     4.7  3  0.041 
+# 2000  0.0     4.8  3  0.041
+
+## Blackbear, truncated season
+bear <- det %>% filter(Month >= 4 & Month <= 10) %>% select(Site, Treatment,Yr_Month, Site_ym, Blackbear, SnowDays, Year, Month, Dist2water_km, low250,low500,low750,low1000,low1250, low1500, low1750, low2000)
+
+### Blackbear models
+Blackbear.0 <- glmmTMB(Blackbear~1, data = bear, family = nbinom2)
+Blackbear.250 <- glmmTMB(Blackbear~low250, data = bear, family = nbinom2)
+Blackbear.500 <- glmmTMB(Blackbear~low500, data = bear, family = nbinom2)
+Blackbear.750 <- glmmTMB(Blackbear~low750, data = bear, family = nbinom2)
+Blackbear.1000 <- glmmTMB(Blackbear~low1000, data = bear, family = nbinom2)
+Blackbear.1250 <- glmmTMB(Blackbear~low1250, data = bear, family = nbinom2)
+Blackbear.1500 <- glmmTMB(Blackbear~low1500, data = bear, family = nbinom2)
+Blackbear.1750 <- glmmTMB(Blackbear~low1750, data = bear, family = nbinom2)
+Blackbear.2000 <- glmmTMB(Blackbear~low2000, data = bear, family = nbinom2)
+
+## Model selection with AICctab (bbmle) --> exluding VegHt
+modnames <- c("NULL","250m", "500m", "750m", "1000m", "1250m", "1500m", "1750m", "2000m")
+Blackbeartab <- ICtab(Blackbear.0,Blackbear.250,Blackbear.500,Blackbear.750,Blackbear.1000,Blackbear.1250, Blackbear.1500, Blackbear.1750, Blackbear.2000, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
+Blackbeartab
+
+#    dLogLik dAIC df weight
+# 500m  2.6     0.0  3  0.295 
+# 2000m 1.9     1.4  3  0.150 
+# 250m  1.5     2.2  3  0.097 
+# 1000m 1.4     2.3  3  0.091 
+# 1750m 1.4     2.4  3  0.087 
+# 1500m 1.3     2.6  3  0.081 
+# 750m  1.2     2.8  3  0.073 
+# 1250m 1.1     2.9  3  0.068 
+# NULL  0.0     3.2  2  0.059 
