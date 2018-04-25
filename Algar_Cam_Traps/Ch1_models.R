93141812498f5db426e1a02081ea43c9de5a6906
ETattersall <ertattersall@gmail.com>
2018-04-24 17:12:55 -0700

    Deciding on random effects to include, exploring model residuals

--- Algar_Cam_Traps/Ch1_models.R
+++ Algar_Cam_Traps/Ch1_models.R
@@ -51,10 +51,39 @@ sp <- read.csv("Spatial_covariates.csv")
 det$VegHt <- sp$VegHt[match(det$Site, sp$Site)]
 det$LineWidth <- sp$LineWidth[match(det$Site, sp$Site)]
 
+hist(det$Caribou)
+hist(det$SnowDays)
+hist(det$low1750)
+hist(det$LD1750)
+hist(det$VegHt)
+hist(det$LineWidth)
+
+
+## Visualising patterns using predict function (predicting new data, given the best-fit model)
+## Need to first remove NAs from explanatory variables
+newdat <-  det[ , c(1,2,12:34)] #Naming columns in df
+newdat <- na.omit(newdat) ## Exluding NA rows, which causes lengths to differ
 
 #### Caribou models ####
 ## Lowland measured at 1750m, LD measured at 1750m
 # Not including Dist2Water --> no corresponding hypothesis
+
+#### Finding random structure (using 'beyond optimal' - global- model)
+r0 <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays, zi = ~1, data = det, family = nbinom2)
+rSite <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Site), zi = ~1, data = det, family = nbinom2) 
+rMonth <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Month), zi = ~1, data = det, family = nbinom2)
+r2 <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+r3 <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (Treatment|Site) + (1|Month), zi = ~1, data = det, family = nbinom2) #Will not converge
+summary(r3)#NAs
+
+ICtab(r0, rSite, rMonth, r2, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
+#       dLogLik dAIC df weight
+# r2      8.0     0.0 16 0.6817
+# rSite   6.2     1.5 15 0.3141
+# rMonth  1.3    11.3 15 0.0025
+# r0      0.0    11.9 14 0.0018
+
+### Continue with 2 random effects
 
 #### I. Environmental models ####
 #1. Null model
@@ -121,25 +150,79 @@ CH11 <- glmmTMB(Caribou~ Treatment + VegHt + LD1750 + low1750 + SnowDays + (1|Si
 #12. Global model - Treatment, Line density, VegHt, LineWidth
 CH12 <-  glmmTMB(Caribou~ Treatment + LineWidth + LD1750 + VegHt + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
 
+## Interaction terms
+# Treatment*low1750
+CH13 <-  glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
 
-modnames <- c("NULL", "CH1", "CH2", "CH3", "CH4", "CH5", "CH6", "CH7", "CH8", "CH9", "CH10", "CH11", "CH12")
-cabtab <- ICtab(CH0, CH1, CH2, CH3, CH4, CH5, CH6, CH7, CH8, CH9, CH10, CH11, CH12, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
+
+modnames <- c("NULL", "CH1", "CH2", "CH3", "CH4", "CH5", "CH6", "CH7", "CH8", "CH9", "CH10", "CH11", "CH12", "CH13")
+cabtab <- ICtab(CH0, CH1, CH2, CH3, CH4, CH5, CH6, CH7, CH8, CH9, CH10, CH11, CH12, CH13, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
 cabtab
 #     dLogLik dAIC df weight
-# CH2  13.8     0.0 8  0.157 
-# CH10 17.8     0.1 12 0.153 
-# CH11 17.6     0.3 12 0.133 
-# CH12 18.6     0.4 13 0.127 
-# CH5  16.4     0.9 11 0.101 
-# CH4  13.3     1.0 8  0.095 
-# CH8  16.2     1.2 11 0.085 
-# CH9  17.1     1.4 12 0.077 
+# CH2  13.8     0.0 8  0.156 
+# CH10 17.8     0.1 12 0.152 
+# CH11 17.6     0.3 12 0.131 
+# CH12 18.6     0.4 13 0.126 
+# CH5  16.4     0.9 11 0.100 
+# CH4  13.3     1.0 8  0.094 
+# CH8  16.2     1.2 11 0.084 
+# CH9  17.1     1.4 12 0.076 
 # CH6  14.0     1.6 9  0.070 
+# CH13 19.1     5.4 16 0.010 
 # CH7   5.5    22.7 11 <0.001
 # CH3   2.3    23.1 8  <0.001
 # CH1   3.5    24.7 10 <0.001
 # NULL  0.0    25.6 7  <0.001
 
 summary(CH12)
+summary(CH3) 
+
+# Comparing all models together (environmental and linear characteristic)
+modnames <- c("NULL", "C.2", "C.3", "C.4", "CH0", "CH1", "CH2", "CH3", "CH4", "CH5", "CH6", "CH7", "CH8", "CH9", "CH10", "CH11", "CH12", "CH13")
+cabtab <- ICtab(C.1, C.2, C.3, C.4, CH0, CH1, CH2, CH3, CH4, CH5, CH6, CH7, CH8, CH9, CH10, CH11, CH12, CH13, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
+cabtab
+
+lrtest(CH2,CH10)
+
+# Model 1: Caribou ~ VegHt + low1750 + SnowDays + (1 | Site) + (1 | Month)
+# Model 2: Caribou ~ Treatment + LineWidth + LD1750 + low1750 + SnowDays + 
+#  (1 | Site) + (1 | Month)
+ #   Df  LogLik Df  Chisq Pr(>Chisq)  
+# 1   8 -265.60                       
+# 2  12 -261.63  4 7.9497    0.09344 .
+
+lrtest(CH10,CH11) #Line width significantly better than veg ht, but only a tiny tiny (0.14) difference in neg.LogLik
+
+summary(CH10)
+summary(CH2)
+summary(CH12)
 
 
+## Residual plot for global model(CH12)
+op <- par(mfrow = c(1,2))
+pred <- predict(CH12, zitype = "response")
+resid <- residuals(CH12)
+
+## Plot residuals against predicted --> diagonal lines characteristic of discrete data
+plot(pred, resid, main = "Residuals vs. Predicted", xlab = "Predicted Values", ylab = "Residuals")
+abline(h = 0, lty = 2)  
+lines(smooth.spline(pred, resid))
+plot(fitted(CH12), residuals(CH12), main= "Residuals vs Fitted", xlab = "Fitted Values", ylab = "Residuals")
+abline(h = 0, lty = 2)
+
+#### Residual plot for most parsimonious model (CH2)
+op <- par(mfrow = c(1,2))
+pred <- predict(CH2, zitype = "response")
+resid <- residuals(CH2)
+
+## Plot residuals against predicted --> diagonal lines characteristic of discrete data
+plot(pred, resid, main = "Residuals vs. Predicted", xlab = "Predicted Values", ylab = "Residuals")
+abline(h = 0, lty = 2)  
+lines(smooth.spline(pred, resid))
+plot(fitted(CH2), residuals(CH2), main= "Residuals vs Fitted", xlab = "Fitted Values", ylab = "Residuals")
+abline(h = 0, lty = 2)
+
+## Global model is a better fit, which is expected given that it hase more parameters
+
+
+## Standardizing estimates? Evidence ratios? Model averaging?
