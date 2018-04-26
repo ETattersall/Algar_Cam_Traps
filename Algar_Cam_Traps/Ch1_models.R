54b98cd3506527e55bff73843285f921516326f3
ETattersall <ertattersall@gmail.com>
2018-04-26 16:15:25 -0700

    Re-running caribou models with nbinom1 distribution. Still need to check residuals

--- Algar_Cam_Traps/Ch1_models.R
+++ Algar_Cam_Traps/Ch1_models.R
@@ -68,12 +68,75 @@ newdat <- na.omit(newdat) ## Exluding NA rows, which causes lengths to differ
 ## Lowland measured at 1750m, LD measured at 1750m
 # Not including Dist2Water --> no corresponding hypothesis
 
+### Exploring distribution and overdispersion (code from Zuur & Ieno, 2016)
+##1. Proportion of 0's in data
+table(is.na(det$Caribou)) #647 NAs, 853 non-NAs
+table(det$Caribou == 0) #781
+sum(det$Caribou==0, na.rm = TRUE)/nrow(det) #52% 0's in data
+
+##2. Fitting most basic model - Poisson GLM - to global model 
+glm1 <- glmmTMB(Caribou~ Treatment + LineWidth + LD1750 + VegHt + low1750 + SnowDays + (1| Site) + (1|Month), data = det, family = poisson)
+# Residuals and overdispersion
+E1 <- resid(glm1, type="pearson")
+N <- nrow(det)
+p <- length(coef(glm1))
+sum(E1^2)/(N-p) #0.815 --> less than one, so not overdispersed
+
+##3. Simulating data from the model
+#function for calculating overdispersion (as above)
+MyDispersion <- function(y, mu, N, p){
+  e <- (y-mu)/sqrt(mu)
+  sum(e^2)/(N-p)
+}
+muFit <- fitted(glm1) #fitted values from model
+Ysim <- matrix(nrow = N, ncol = 100000) #matrix storing simulated responses
+DispersionSim <- vector(length = 100000)#vector storing Dispersion value
+#Loop running simulation of Poisson distributed data and calculating Dispersion value
+for(i in 1:100000){
+  Ysim[,i] <- rpois(N,lambda = muFit)
+  DispersionSim[i] <- MyDispersion(Ysim[,i], muFit, N, p) #warnings generated but vector obtained
+}
+#Histogram of dispersion statistics
+hist(DispersionSim)# Simulated statistics are mostly on either side of 1 -->unlikely to have overdispersion
+
+## Comparing proportions of 0's in simulated data to that in sampled data
+zeros <- vector(length = 100000)
+for(i in 1:100000){
+  zeros[i] <- sum(Ysim[,i] == 0) / N
+}
+
+plot(table(zeros),
+     axes = FALSE,
+     xlim = c(0.4, 1),
+     xlab = "Percentage of zeros",
+     ylab = "Frequency")
+
+axis(2)
+axis(1, 
+     at = c(0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
+     labels = c("40%", "50%", "60%", "70%", "80%", "90%"))
+points(x = sum(det$Caribou==0, na.rm = TRUE)/ N,
+       y = 0, pch = 16, cex = 5, col = 2)
+## Model comparisons of distributions and zero-inflation
+glm2 <- glmmTMB(Caribou~ Treatment + LineWidth + LD1750 + VegHt + low1750 + SnowDays + (1| Site) + (1|Month), data = det, family = nbinom1(link= "log"))
+glm3 <- glmmTMB(Caribou~ Treatment + LineWidth + LD1750 + VegHt + low1750 + SnowDays + (1| Site) + (1|Month), data = det, family = nbinom2(link = "log"))
+glm4 <- glmmTMB(Caribou~ Treatment + LineWidth + LD1750 + VegHt + low1750 + SnowDays + (1| Site) + (1|Month), zi = ~1, data = det, family = poisson)
+glm5 <- glmmTMB(Caribou~ Treatment + LineWidth + LD1750 + VegHt + low1750 + SnowDays +(1| Site) + (1|Month), zi = ~1, data = det, family = nbinom1(link= "log"))
+glm6 <- glmmTMB(Caribou~ Treatment + LineWidth + LD1750 + VegHt + low1750 + SnowDays + (1| Site) + (1|Month), zi = ~1, data = det, family = nbinom2(link= "log"))
+
+modnames <- c("Poisson", "Nbinom1", "Nbinom2", "ZIP", "ZINB1", "ZINB2")
+ICtab(glm1,glm2,glm3,glm4,glm5,glm6, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
+
+summary(glm2)
+summary(glm5)
+
+
 #### Finding random structure (using 'beyond optimal' - global- model)
-r0 <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays, zi = ~1, data = det, family = nbinom2)
-rSite <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Site), zi = ~1, data = det, family = nbinom2) 
-rMonth <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Month), zi = ~1, data = det, family = nbinom2)
-r2 <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
-r3 <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (Treatment|Site) + (1|Month), zi = ~1, data = det, family = nbinom2) #Will not converge
+r0 <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays, data = det, family = nbinom1)
+rSite <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Site), data = det, family = nbinom1) 
+rMonth <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Month), data = det, family = nbinom1)
+r2 <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
+r3 <- glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (Treatment|Site) + (1|Month), data = det, family = nbinom1) #Will not converge
 summary(r3)#NAs
 
 ICtab(r0, rSite, rMonth, r2, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
@@ -87,102 +150,69 @@ ICtab(r0, rSite, rMonth, r2, type= "AIC", weights = TRUE, delta = TRUE, logLik =
 
 #### I. Environmental models ####
 #1. Null model
-C.1 <- glmmTMB(Caribou~1 + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+E1 <- glmmTMB(Caribou~1 + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #2. Global model
-C.2 <- glmmTMB(Caribou~low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+E2 <- glmmTMB(Caribou~low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #3. Lowland only
-C.3 <- glmmTMB(Caribou~low1750 + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+E3 <- glmmTMB(Caribou~low1750 + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #4. Snow only
-C.4 <- glmmTMB(Caribou~SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+E4 <- glmmTMB(Caribou~SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
-modnames <- c("NULL", "Low + Snow", "Low", "Snow")
-cabtab <- ICtab(C.1, C.2, C.3, C.4, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
-cabtab
-
-#         dLogLik dAIC df weight
-# Low + Snow 21.1     0.0 7  0.924 
-# Low        17.6     5.0 6  0.076 
-# Snow        4.0    32.2 6  <0.001
-# NULL        0.0    38.2 5  <0.001
-
-## Low + Snow in top model, continue hypothesis testing with both
 
 #### Line characteristic models ####
 #0. Null model (using top model from environmental models)
-CH0 <- glmmTMB(Caribou~low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L0 <- glmmTMB(Caribou~low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #1. Treatment
-CH1 <- glmmTMB(Caribou~Treatment + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L1 <- glmmTMB(Caribou~Treatment + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #2. VegHt
-CH2 <- glmmTMB(Caribou~ VegHt + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L2 <- glmmTMB(Caribou~ VegHt + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #3. Line density
-CH3 <- glmmTMB(Caribou~ LD1750 + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L3 <- glmmTMB(Caribou~ LD1750 + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #4. Line Width
-CH4 <- glmmTMB(Caribou~ LineWidth + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L4 <- glmmTMB(Caribou~ LineWidth + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #5. Treatment + LineWidth
-CH5 <- glmmTMB(Caribou~ Treatment + LineWidth + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L5 <- glmmTMB(Caribou~ Treatment + LineWidth + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #6. LineWidth + VegHt
-CH6 <- glmmTMB(Caribou~ LineWidth + VegHt + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L6 <- glmmTMB(Caribou~ LineWidth + VegHt + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #7. Treatment + Line density
-CH7 <- glmmTMB(Caribou~ Treatment + LD1750 + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L7 <- glmmTMB(Caribou~ Treatment + LD1750 + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #8. VegHt + Treatment
-CH8 <- glmmTMB(Caribou~ Treatment + VegHt + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L8 <- glmmTMB(Caribou~ Treatment + VegHt + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #9. Treatment, LineWidth, VegHt
-CH9 <- glmmTMB(Caribou~ Treatment + LineWidth + VegHt + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L9 <- glmmTMB(Caribou~ Treatment + LineWidth + VegHt + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #10. Treatment, LineWidth, Line density
-CH10 <- glmmTMB(Caribou~ Treatment + LineWidth + LD1750 + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L10 <- glmmTMB(Caribou~ Treatment + LineWidth + LD1750 + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #11. Treatment, Line density, VegHt
-CH11 <- glmmTMB(Caribou~ Treatment + VegHt + LD1750 + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L11 <- glmmTMB(Caribou~ Treatment + VegHt + LD1750 + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 #12. Global model - Treatment, Line density, VegHt, LineWidth
-CH12 <-  glmmTMB(Caribou~ Treatment + LineWidth + LD1750 + VegHt + low1750 + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L12 <-  glmmTMB(Caribou~ Treatment + LineWidth + LD1750 + VegHt + low1750 + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 ## Interaction terms
 # Treatment*low1750
-CH13 <-  glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Site) + (1|Month), zi = ~1, data = det, family = nbinom2)
+L13 <-  glmmTMB(Caribou~ LineWidth + LD1750 + VegHt + low1750*Treatment + SnowDays + (1|Site) + (1|Month), data = det, family = nbinom1)
 
 
-modnames <- c("NULL", "CH1", "CH2", "CH3", "CH4", "CH5", "CH6", "CH7", "CH8", "CH9", "CH10", "CH11", "CH12", "CH13")
-cabtab <- ICtab(CH0, CH1, CH2, CH3, CH4, CH5, CH6, CH7, CH8, CH9, CH10, CH11, CH12, CH13, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
-cabtab
-#     dLogLik dAIC df weight
-# CH2  13.8     0.0 8  0.156 
-# CH10 17.8     0.1 12 0.152 
-# CH11 17.6     0.3 12 0.131 
-# CH12 18.6     0.4 13 0.126 
-# CH5  16.4     0.9 11 0.100 
-# CH4  13.3     1.0 8  0.094 
-# CH8  16.2     1.2 11 0.084 
-# CH9  17.1     1.4 12 0.076 
-# CH6  14.0     1.6 9  0.070 
-# CH13 19.1     5.4 16 0.010 
-# CH7   5.5    22.7 11 <0.001
-# CH3   2.3    23.1 8  <0.001
-# CH1   3.5    24.7 10 <0.001
-# NULL  0.0    25.6 7  <0.001
-
-summary(CH12)
-summary(CH3) 
 
 # Comparing all models together (environmental and linear characteristic)
-modnames <- c("NULL", "C.2", "C.3", "C.4", "CH0", "CH1", "CH2", "CH3", "CH4", "CH5", "CH6", "CH7", "CH8", "CH9", "CH10", "CH11", "CH12", "CH13")
-cabtab <- ICtab(C.1, C.2, C.3, C.4, CH0, CH1, CH2, CH3, CH4, CH5, CH6, CH7, CH8, CH9, CH10, CH11, CH12, CH13, mnames = modnames, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
+cabtab <- ICtab(E1, E2, E3, E4, L0, L1, L2, L3, L4, L5, L6, L7, L8, L9, L10, L11, L12, L13, type= "AIC", weights = TRUE, delta = TRUE, logLik = TRUE, sort=TRUE)
 cabtab
 
-lrtest(CH2,CH10)
+lrtest(L2,L10)
 
 # Model 1: Caribou ~ VegHt + low1750 + SnowDays + (1 | Site) + (1 | Month)
 # Model 2: Caribou ~ Treatment + LineWidth + LD1750 + low1750 + SnowDays + 
