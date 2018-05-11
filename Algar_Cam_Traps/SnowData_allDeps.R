a40716d91cca048dbfd007b8502aec2867a80551
ETattersall <ertattersall@gmail.com>
2018-05-11 12:04:00 -0700

    Coding inactive months as 0 instead of NA, using proportion to measure snow instead of snowdays, fixed code that caused duplicates in m.snow

--- Algar_Cam_Traps/SnowData_allDeps.R
+++ Algar_Cam_Traps/SnowData_allDeps.R
@@ -169,6 +169,8 @@ plot(x = m.snow$Yr_Month, y = m.snow$Snow, xlab = "Year-Month",ylab = "Snow Days
 ## 2017-2018 winter has numerous partial months. Address by first checking to see is Active Days and SnowDays are correlated. If so, divide SnowDays by ActiveDays
 ##m.snow has 1214 site-months. Should have (24*30) + (36*18) + (13*6) = 1446, + NAs for months when camera were not deployed = 2190. Discrepancy from failed cameras and CSV errors
 
+### IMPORTANT NOTE: Code inactive months as 0s instead of NAs --> consistent with how camera detection data is coded and models are more likely to run with fewer NAs
+
 ## I. Add NAs for months when cameras were not active
 # a. Second deployment (in lab notebook, June 2, 2017): 
 # Algar18,32,49,50 inactive 2016-12 to 2017-03
@@ -514,7 +516,7 @@ table(is.na(m.snow$Snow))
 
 ## Now have correct NUMBER of rows (excluding months when cameras were not deployed)
 
-
+m.snow[duplicated(m.snow), ] #no duplicates
 
 
 ## III. Add 0s for months when camera was not deployed (12 for Algar25-60, 24 for Algar61-73)
@@ -525,15 +527,19 @@ Station <- paste(Algar, Algarnum, sep="")
 class(Station)
 Station <- as.factor(Station)
 Station <- rep(Station, 12) # 12 = no. months
+#Order Station to avoid causing duplicate rows (e.g. matching all Algar25 with all 2016-11)
+Station <- sort(Station)
+Station
 Yr_Month <- c("2015-11", "2015-12", "2016-01", "2016-02", "2016-03", "2016-04", "2016-05", "2016-06", "2016-07", "2016-08", "2016-09", "2016-10")
 Yr_Month <- rep(Yr_Month, 36) #36 = no. of cameras
-Snow <- rep(0,432)
+Snow <- rep(NA,432)
 # Combine into data frame of NA snow months
 NAmonths <- cbind.data.frame(Station,Yr_Month, Snow)
 ## Add NA months to m.snow
 m.snow <- rbind.data.frame(NAmonths,m.snow)
 
 m.snow <- with(m.snow, m.snow[order(as.factor(as.character(Station)), as.factor(as.character(Yr_Month))), ])
+m.snow[duplicated(m.snow),]
 head(m.snow)
 tail(m.snow)
 
@@ -544,18 +550,23 @@ Station <- paste(Algar, Algarnum, sep="")
 class(Station)
 Station <- as.factor(Station)
 Station <- rep(Station, 24) # 24 = no. months
+#Order Station to avoid causing duplicate rows (e.g. matching all Algar25 with all 2016-11)
+Station <- sort(Station)
+Station
 Yr_Month <- c("2015-11", "2015-12", "2016-01", "2016-02", "2016-03", "2016-04", "2016-05", "2016-06", "2016-07", "2016-08", "2016-09", "2016-10", "2016-11", "2016-12", "2017-01", "2017-02", "2017-03", "2017-04", "2017-05", "2017-06", "2017-07", "2017-08", "2017-09", "2017-10")
 Yr_Month <- rep(Yr_Month, 13) #13 = no. of cameras
-Snow <- rep(0,312)
+Snow <- rep(NA,312)
 # Combine into data frame of NA snow months
 NAmonths <- cbind.data.frame(Station,Yr_Month, Snow)
 ## Add NA months to m.snow
 m.snow <- rbind.data.frame(NAmonths,m.snow)
 
 m.snow <- with(m.snow, m.snow[order(as.factor(as.character(Station)), as.factor(as.character(Yr_Month))), ])
+m.snow[duplicated(m.snow), ]
 head(m.snow)
 tail(m.snow)
 table(is.na(m.snow$Snow))
+m.snow[duplicated(m.snow), ]
 
 #### Add to monthly detection data sets - MonthlyDetections_nov2015-apr2018.csv AND Seismic_nov2015-apr2018.csv ####
 ## Check correlation with ActiveDays
@@ -568,22 +579,94 @@ summary(DF)
 ## Create Site_ym column in m.snow
 m.snow$Site_ym <- paste(m.snow$Station,m.snow$Yr_Month, sep=" ")
 
+## Add ActiveDays to m.snow, calculate, proportion of snowdays, save, then re-script m.snow with NAs instead of zeroes to compare
+m.snow$ActiveDays <- DF$ActiveDays[match(m.snow$Site_ym, DF$Site_ym)]
+
+m.snow$pSnow <- m.snow$Snow/m.snow$ActiveDays
+summary(m.snow$pSnow) #977 NAs
+write.csv(m.snow, "SnowDays_permonth_NAs.csv")
+table(m.snow$pSnow) #shouldn't have values >1, but do (rectified)
+m.snow[which(m.snow$pSnow>1), ] #Counts that are different by 1 is an accounting error, ActiveDays should be adjusted (in some cases SnowDays; need to assess each case independently. See lab notebook for May 11,2018) ---> Rectified in dataframes
+
+#Converting NAs in Snow to 0s
+ms <- m.snow[ ,1:5]
+table(is.na(ms$Snow)) #970 TRUE
+summary(ms$Snow)
+ms$Snow[is.na(ms$Snow)] <- 0
+summary(ms$Snow) #No NAs, but summer months have equivalent value to fail months
+table(is.na(ms$Snow)) #FALSE 2190
+table(ms$Snow==0) ## 1299 TRUE--> Both No Snow months and NA months
+m.snow$Site_ym[duplicated(m.snow$Site_ym)]
+
+ms$pSnow <- ms$Snow/ms$ActiveDays
+ms[duplicated(ms),]
+summary(ms$pSnow) #968 NAs --> only 9 fewer than coding as NAs
+
+plot(ms$ActiveDays,ms$pSnow) ## No correlation
+
+
+#### Test model with pSnow from NA-coded snow
+## Add SnowDays to DF
 DF$SnowDays <- m.snow$Snow[match(DF$Site_ym, m.snow$Site_ym)]
+summary(DF$SnowDays)
 
 plot(DF$ActiveDays,DF$SnowDays)
-S1 <- lm(SnowDays~ActiveDays,data=DF)
-summary(S1) #low R^2, but ActiveDays has significantly positive effect on SnowDays.
+A <- lm(SnowDays~ActiveDays,data=DF)
+summary(A) #low R^2, but ActiveDays has significantly positive effect on SnowDays.
 
-## Divide SnowDays/ActiveDays for a proportional covariate
-DF$pSnow <- DF$SnowDays/DF$ActiveDays
+## Add pSnow to DF
+DF$pSnow <- m.snow$pSnow[match(DF$Site_ym, m.snow$Site_ym)]
 summary(DF$pSnow)
 
-seis.snow <- m.snow[1:1800,]
-head(seis.snow)
-tail(seis.snow)
-summary(seis.snow$Station)
+## Add SnowDays and pSnow to seis
+seis$SnowDays <- m.snow$Snow[match(seis$Site_ym, m.snow$Site_ym)]
+seis$pSnow <- m.snow$pSnow[match(seis$Site_ym, m.snow$Site_ym)]
 
-seis$SnowDays <- seis.snow$Snow[match(seis$Site_ym, seis.snow$Site_ym)]
+library(glmmTMB)
+library(bbmle)
+#Compare results with pSnow and SnowDays
+L13 <- glmmTMB(Caribou~ Treatment + pSnow + (1|Site) + (1|Month), data = seis, zi=~ActiveDays, family = nbinom1)
+summary(L13)
+L14 <- glmmTMB(Caribou~ Treatment + SnowDays + (1|Site) + (1|Month), data = seis, zi=~ActiveDays, family = nbinom1)
+summary(L14)
+## Same significance level, different parameter estimates
+
+
+#### Test model with pSnow from 0-coded snow
+## Add SnowDays to DF
+DF$SnowDays <- ms$Snow[match(DF$Site_ym, ms$Site_ym)]
+summary(DF$SnowDays)
+
+plot(DF$ActiveDays,DF$SnowDays)
+A <- lm(SnowDays~ActiveDays,data=DF)
+summary(A) # Higher R^2 than when coded as NAs (makes sense as 0s in Snow correlate with 0s in ActiveDays)
+
+## Add pSnow to DF
+DF$pSnow <- ms$pSnow[match(DF$Site_ym, ms$Site_ym)]
+summary(DF$pSnow)
+A1 <- lm(pSnow~ActiveDays, data=DF)
+summary(A1) # R^2= 0.007
+plot(DF$ActiveDays,DF$pSnow)
+
+## Add SnowDays and pSnow to seis
+seis$SnowDays <- ms$Snow[match(seis$Site_ym, ms$Site_ym)]
+seis$pSnow <- ms$pSnow[match(seis$Site_ym, ms$Site_ym)]
+
+
+#Compare results with pSnow and SnowDays
+L15 <- glmmTMB(Caribou~ Treatment + pSnow + (1|Site) + (1|Month), data = seis, zi=~ActiveDays, family = nbinom1)
+summary(L13)
+L16 <- glmmTMB(Caribou~ Treatment + SnowDays + (1|Site) + (1|Month), data = seis, zi=~ActiveDays, family = nbinom1)
+summary(L14)
+
+## Models run with both NA-coded fail months and 0-coded fail months, when Snow is measured both as a total number of days and as a proportion of ActiveDays
+## I will continue to measure Snow as a proportion because it accounts for unequal days per month and accounts for differences in ActiveDays (removes correlation)
+## I will continue to code fail months as 0s instead of NA to remain consistent with camera detection data
+
+summary(DF$SnowDays) #No NAs
+summary(DF$pSnow)
+summary(seis$SnowDays)
+summary(seis$pSnow)
 
 write.csv(DF, "MonthlyDetections_nov2015-apr2018.csv")
 write.csv(seis, "Seismic_nov2015-apr2018.csv")
