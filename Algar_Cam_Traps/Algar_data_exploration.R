89ca0325829d1d110b08dfb1b0f9b59477f49206
ETattersall <ertattersall@gmail.com>
2018-05-15 12:56:11 -0700

    Updated with full dataset

--- Algar_Cam_Traps/Algar_data_exploration.R
+++ Algar_Cam_Traps/Algar_data_exploration.R
@@ -2,6 +2,7 @@
 # Algar_data_exploration.R
 # Checking for collinearities, outliers, 0's, etc (see Zuur et al., 2010)
 # Started Jan. 30, 2018
+# Updated May10, 2018 with full dataset
 ######################################
 
 
@@ -16,30 +17,53 @@ library(tidyr) #for data management (gather function)
 getwd()
 
 setwd("C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Data")
-dat <- read.csv("2015.01_monthlydetections.csv") # First deployment monthly detection data
-head(dat)
-dat$X.1 <- NULL
-dat$X <- NULL
+det <- read.csv("Seismic_nov2015-apr2018.csv") # Detection deta for 30 months of surveying
+head(det)
+det$X.2 <- NULL
+det$X.1 <- NULL
+det$X <- NULL
 
-#Loading % lowland data
-low <- read.csv("Lowlandcover_9buffersizes.csv")
-# Forgot to include 750m scale. Not really imp. as there is little diff. across scales
-prop4areas <- cbind.data.frame(low$Prop250, low$Prop500, low$Prop1000, low$Prop2000)
-colnames(prop4areas) <- c("250m", "500m", "1000m", "2000m")
-prop4areas$Station <- low$CamStation
-summary(prop4areas) #approximately the same
+#Load lowland data
+low <- read.csv("newAVIE_lowland8buffersizes.csv")
 
-##Add 250m and 500m to monthly detections
-dat$low250 <- low$Prop250[match(dat$Site, low$CamStation)]
-dat$low500 <- low$Prop500[match(dat$Site, low$CamStation)]
+#Combine data
+det$low250 <- low$low250[match(det$Site, low$CamStation)]
+det$low500 <- low$low500[match(det$Site, low$CamStation)]
+det$low750 <- low$low750[match(det$Site, low$CamStation)]
+det$low1000 <- low$low1000[match(det$Site, low$CamStation)]
+det$low1250 <- low$low1250[match(det$Site, low$CamStation)]
+det$low1500 <- low$low1500[match(det$Site, low$CamStation)]
+det$low1750 <- low$low1750[match(det$Site, low$CamStation)]
+det$low2000 <- low$low2000[match(det$Site, low$CamStation)]
+
+#Load linedensity data
+LineDens <- read.csv("AlgarStationsLD_Lines.csv")
+
+# Combine datasets
+det$LD250 <- LineDens$X250m[match(det$Site, LineDens$CamStation)]
+det$LD500 <- LineDens$X500m[match(det$Site, LineDens$CamStation)]
+det$LD750 <- LineDens$X750m[match(det$Site, LineDens$CamStation)]
+det$LD1000 <- LineDens$X1000m[match(det$Site, LineDens$CamStation)]
+det$LD1250 <- LineDens$X1250m[match(det$Site, LineDens$CamStation)]
+det$LD1500 <- LineDens$X1500m[match(det$Site, LineDens$CamStation)]
+det$LD1750 <- LineDens$X1750m[match(det$Site, LineDens$CamStation)]
+det$LD2000 <- LineDens$X2000m[match(det$Site, LineDens$CamStation)]
+
+
+#Load line width and veg height
+sp <- read.csv("Spatial_covariates.csv")
+
+det$VegHt <- sp$VegHt[match(det$Site, sp$Site)]
+det$LineWidth <- sp$LineWidth[match(det$Site, sp$Site)]
+
 
 #### Step 1. Check outliers ####
 
 #### Species: gather species together for boxplot for every species ####
-d1 <- gather(data = dat, key = Species, value = Detections, 5:11)
+d1 <- gather(data = det, key = Species, value = Detections, 5:11)
 
 #ggplot code for boxplot
-bp <- ggplot(data = d1, aes(x = Species, y = Detections, fill = Species))
+bp <- ggplot(data = d1, aes(x = Species, y = Detections, fill = Species)) #Coyote has one major outlier, probably Algar17 2017-08
 bp + geom_boxplot() + theme_classic() + theme(legend.position = "none")
 
 #dot plot
@@ -54,19 +78,7 @@ dp + stat_summary(fun.y=mean, geom="point", shape=18,
 #### Step 2: Checking for homogeneity of variance ####
 # If we are interested in effect of treatment on species detections, plot this in box plot. Is variation from mean similar across groups?
 
-# For pilot data
-ggplot(data = d1, aes(x = Treatment, y = Detections, fill = Treatment)) + geom_boxplot() + theme_classic() + xlab("Sampling Strata") + ylab("Detections/Month") + scale_x_discrete(limits=c("Control", "SPP")) + scale_fill_manual(values=c("orange", "purple")) + guides(fill = guide_legend(title = NULL)) + facet_wrap( ~ Species, ncol = 2, scales = "free_y") + theme(legend.position = "none") +theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 45, hjust = 1, colour = "black", size = 12)) + theme(axis.title.x = element_text(angle = 0, colour = "black", size = 14)) + theme(axis.title.y = element_text(angle = 90, colour = "black", size = 14)) + theme(strip.text = element_text(colour = "black", size = 14)) + theme(aspect.ratio=1)
-
-# For all detection data
-full <- read.csv("MonthlyDetections_nov2015-nov2017.csv")
-full$X <- NULL
-f1 <- gather(data = full, key = Species, value = Detections, 5:11)
-
-ggplot(data = f1, aes(x = Treatment, y = Detections, fill = Treatment)) + geom_boxplot() + theme_classic() + xlab("Sampling Strata") + ylab("Detections/Month") + scale_x_discrete(limits=c("HumanUse", "Control", "SPP", "NatRegen")) + scale_fill_manual(values=c("red", "light green", "orange", "purple")) + guides(fill = guide_legend(title = NULL)) + facet_wrap( ~ Species, nrow=3, scales = "free_y") + theme(legend.position = "none") +theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 45, hjust = 1, colour = "black", size = 12)) + theme(axis.title.x = element_text(angle = 0, colour = "black", size = 14)) + theme(axis.title.y = element_text(angle = 90, colour = "black", size = 14)) + theme(strip.text = element_text(colour = "black", size = 14)) + theme(aspect.ratio=1)
-
-
-
-
+ggplot(data = d1, aes(x = Treatment, y = Detections, fill = Treatment)) + geom_boxplot() + theme_classic() + xlab("Sampling Strata") + ylab("Detections/Month") + scale_x_discrete(limits=c("Control", "HumanUse", "SPP", "NatRegen")) + scale_fill_manual(values=c("orange","red", "purple", "green")) + guides(fill = guide_legend(title = NULL)) + facet_wrap( ~ Species, ncol = 2, scales = "free_y") + theme(legend.position = "none") +theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 45, hjust = 1, colour = "black", size = 12)) + theme(axis.title.x = element_text(angle = 0, colour = "black", size = 14)) + theme(axis.title.y = element_text(angle = 90, colour = "black", size = 14)) + theme(strip.text = element_text(colour = "black", size = 14)) + theme(aspect.ratio=1)
 
 
 #### Levene's test for equal variance. Tests the null hypothesis that variance are equal ####
@@ -74,39 +86,39 @@ ggplot(data = f1, aes(x = Treatment, y = Detections, fill = Treatment)) + geom_b
 install.packages("Rcmdr")
 library(Rcmdr)
 
-wolf.lm <- lm(data = dat, Wolf~Treatment)
-leveneTest(wolf.lm) #p = 0.044 --> unequal variance
+wolf.lm <- lm(data = det, Wolf~Treatment)
+leveneTest(wolf.lm) #p = 0.234 --> equal variance
 
 #Blackbear
-Blackbear.lm <- lm(data = dat, Blackbear~Treatment)
-leveneTest(Blackbear.lm) #p = 0.191 --> equal variance
+Blackbear.lm <- lm(data = det, Blackbear~Treatment)
+leveneTest(Blackbear.lm) #p = 0.0024--> unequal variance
 
 #Caribou
-Caribou.lm <- lm(data = dat, Caribou~Treatment)
-leveneTest(Caribou.lm) #p = 0.010
+Caribou.lm <- lm(data = det, Caribou~Treatment)
+leveneTest(Caribou.lm) #p = 1.51e-06
 
 #WTDeer
-WTDeer.lm <- lm(data = dat, WTDeer~Treatment)
-leveneTest(WTDeer.lm) # p = 1.98e-05
+WTDeer.lm <- lm(data = det, WTDeer~Treatment)
+leveneTest(WTDeer.lm) # p = 2.2e-16
 
 #Moose
-Moose.lm <- lm(data = dat, Moose~Treatment)
-leveneTest(Moose.lm) #p = 0.322
+Moose.lm <- lm(data = det, Moose~Treatment)
+leveneTest(Moose.lm) #p = 0.0080
 
 #Coyote
-Coyote.lm <- lm(data = dat, Coyote~Treatment)
-leveneTest(Coyote.lm) #p = 0.028
+Coyote.lm <- lm(data = det, Coyote~Treatment)
+leveneTest(Coyote.lm) #p = 0.117
 
 #Lynx
-Lynx.lm <- lm(data = dat, Lynx~Treatment)
-leveneTest(Lynx.lm) #p = 0.430
+Lynx.lm <- lm(data = det, Lynx~Treatment)
+leveneTest(Lynx.lm) #p = 0.0038
 
 #### Residuals plotting for explanatory variables that are continuous (SnowDays, low500) ####
-Wolf.snow <- lm(Wolf~SnowDays, data = dat)
+Wolf.snow <- lm(Wolf~SnowDays, data = det)
 plot(Wolf.snow)  #Residuals not centred around zero --> unequal variance. Mean residuals = slightly negative. Makes sense because the mean detection is slightly above zero, but most points are zero
-qqnorm(dat$Wolf) #We know data is non-normal, as it is count data
+qqnorm(det$Wolf) #We know data is non-normal, as it is count data
 
-Bear.snow <- lm(Blackbear~SnowDays, data = dat)
+Bear.snow <- lm(Blackbear~SnowDays, data = det)
 plot(Bear.snow)
 
 Caribou.snow <- lm(Caribou~SnowDays, data = dat)
