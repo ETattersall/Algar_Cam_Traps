32358ceb85b9f895c21648e7f8317b35591afe75
ETattersall <ertattersall@gmail.com>
2018-10-08 14:30:21 -0700

    Adding residual analysis and temporal plots

--- Algar_Cam_Traps/Algar_analyses/Ch3_BlackbearHyp_modelling.R
+++ Algar_Cam_Traps/Algar_analyses/Ch3_BlackbearHyp_modelling.R
@@ -26,21 +26,77 @@ Bears_sc$Prey <- ifelse(Bears_sc$Prey>0,1,0)
 table(Bears_sc$Prey) ## 519 occurrences of Prey
 
 ##B0 Core Model
-B0 <- glmmTMB(Blackbear~pOpen250 + (1|Site), data = Bears_sc, zi~1, family = "binomial")
+B0 <- glmmTMB(Blackbear~pOpen250 + (1|Site), data = Bears_sc, family = "binomial")
 
 ## B1 Competition/ Facilitation hypotheses
-B1 <- glmmTMB(Blackbear~Wolf + pOpen250 + (1|Site), data = Bears_sc, zi~1, family = "binomial")
+B1 <- glmmTMB(Blackbear~Wolf + pOpen250 + (1|Site), data = Bears_sc, family = "binomial")
 
 ## B1 with LD
-B1.LD1500 <- glmmTMB(Blackbear~Wolf*LD1500 + pOpen250 + (1|Site), data = Bears_sc, zi~1, family = "binomial")
+B1.LD1500 <- glmmTMB(Blackbear~Wolf*LD1500 + pOpen250 + (1|Site), data = Bears_sc, family = "binomial")
 
 ## B2 Predation
-B2 <- glmmTMB(Blackbear~Prey + pOpen250 + (1|Site), data = Bears_sc, zi~1, family = "binomial")
+B2 <- glmmTMB(Blackbear~Prey + pOpen250 + (1|Site), data = Bears_sc,  family = "binomial")
 
 ## B2 with LD
-B2.LD1500 <- glmmTMB(Blackbear~Prey*LD1500 + pOpen250 + (1|Site), data = Bears_sc, zi~1, family = "binomial")
+B2.LD1500 <- glmmTMB(Blackbear~Prey*LD1500 + pOpen250 + (1|Site), data = Bears_sc, family = "binomial")
 
 ##B3 Predation (separated)
-B3 <- glmmTMB(Blackbear~WTDeer + Moose + Caribou + pOpen250 + (1|Site), data = Bears_sc, zi~1, family = "binomial")
+B3 <- glmmTMB(Blackbear~WTDeer + Moose + Caribou + pOpen250 + (1|Site), data = Bears_sc, family = "binomial")
 
 ## B3 with LD??
+
+
+#### Model validation with dHARMa
+devtools::install_github(repo = "florianhartig/DHARMa", subdir = "DHARMa", 
+                         dependencies = T, build_vignettes = T)
+
+library(DHARMa)
+
+
+## Simulate residuals with DHARMa
+res <- simulateResiduals(B1)
+res.LD <- simulateResiduals(B1.LD1500)
+
+## Qualtitative check with plots
+plot(res) ## qq plot shows deviation from expected, residuals vs. predicted with quantile lines --> ideally lines are straight, horizontal, and at 0.25, 0.5, 0.75. BUT deviations can be expected even in good models
+
+## Plot against other predictors
+plotResiduals() ## objects of different lengths...
+
+recalc.Occ <- recalculateResiduals(res, group = (Occ_sc$Site), aggregateBy = sum)
+
+## residuals with glmmTMB
+res.glmmTMB <- residuals(B1)
+length(res.glmmTMB)
+
+plot(fitted(B1), resid(B1), xlab = "Fitted values", ylab = "Bear residuals")
+
+## base R plot of residuals
+Bears_naomit <- na.omit(Bears_sc) ## now same number of observations as residuals
+plot(Bears_naomit$Coyote, res.glmmTMB, xlab = "Wolf Occurrence", ylab = "Bear residuals") ## uneven spread of residuals -- should there be roughly equal points in all corners?
+
+
+plot(Bears_naomit$pOpen250, res.glmmTMB, xlab = "Open forest", ylab = "Bear residuals")
+
+
+recalc.Bears <- recalculateResiduals(res, group = (Bears_sc$Site), aggregateBy = sum)
+
+
+## Formal goodness of fit tests
+#Kolmorgorov-Smirnov test for uniformity
+testUniformity(res) # D = 0.0088 , max. absolute difference between sim. and observed, p-value = prob. of finding D if observed and sim. are same
+## p = 0.229 --> simulated values are not significantly different from observed
+
+
+## Checking temporal autocorrelation
+library(ggplot2)
+
+Bears_sc$Datep <- Bears$Datep[match(Bears_sc$StudyDay, Bears$StudyDay)]
+class(Bears_sc$Datep)
+
+Bears_sc$Datep <- as.Date(Bears_sc$Datep)
+
+time <- ggplot(data = Bears_sc, aes(x = Datep, y = Blackbear, color = Site)) + geom_point() + scale_x_date()
+time
+
+## Difficult to see relationships when plotting all together -- check a few individually?
--- Algar_Cam_Traps/Algar_analyses/Ch3_CoyoteHyp_modelling.R
+++ Algar_Cam_Traps/Algar_analyses/Ch3_CoyoteHyp_modelling.R
@@ -3,6 +3,7 @@
 
 library(glmmTMB)
 library(bbmle)
+library(dplyr)
 
 setwd("C:/Users/ETattersall/Desktop/Algar_Cam_Traps/Algar_Camera_Traps/Data")
 
@@ -62,3 +63,61 @@ Coyote.tab <- ICtab(C0, C1, C1.season, C1.LD1500, C2, C2.season, C2.LD1500, C3, 
 Coyote.tab
 
 summary(C2.season)
+
+### model validation
+
+library(DHARMa)
+
+
+## Simulate residuals with DHARMa
+res <- simulateResiduals(C2.season)
+
+## residuals with glmmTMB
+res.glmmTMB <- residuals(C2.season)
+length(res.glmmTMB)
+
+plot(fitted(C2.season), resid(C2.season), xlab = "Fitted values", ylab = "Coyote*snow residuals")
+
+## Qualtitative check with plots
+plot(res) ## qq plot shows deviation from expected, residuals vs. predicted with quantile lines --> ideally lines are straight, horizontal, and at 0.25, 0.5, 0.75. BUT deviations can be expected even in good models
+
+## base R plot of residuals
+Occ_naomit <- na.omit(Occ_sc) ## now same number of observations as residuals
+plot(Occ_naomit$Coyote, res.glmmTMB, xlab = "Lynx occurrence", ylab = "Coyote*snow residuals")
+plot(Occ_naomit$pOpen1750, res.glmmTMB, xlab = "Open forest", ylab = "Coyote*snow residuals")
+
+plot(Occ_naomit$Snow, res.glmmTMB, xlab = "Snow presence", ylab = "Coyote*snow residuals")
+
+## Formal goodness of fit tests
+#Kolmorgorov-Smirnov test for uniformity
+testUniformity(res) # D = 0.00897 , max. absolute difference between sim. and observed, p-value = prob. of finding D if observed and sim. are same. p = 0.0116
+
+### Plotting each site over time?
+library(ggplot2)
+
+Occ_sc$Datep <- Occ$Datep[match(Occ_sc$StudyDay, Occ$StudyDay)]
+class(Occ_sc$Datep)
+
+Occ_sc$Datep <- as.Date(Occ_sc$Datep)
+
+time <- ggplot(data = Occ_sc, aes(x = Datep, y = Coyote, color = Site)) + geom_point() + scale_x_date()
+time
+
+
+### break into time chunks of 3 months to look for spatiotemporal correlation (determine temporal scales)
+
+time + scale_x_date(limits = as.Date(c("2015-11-01", "2016-02-01"))) ## Algar 17 - 2 days apart, 18 days, 7 days, 11 days, 5 days,
+time + scale_x_date(limits = as.Date(c("2016-02-01", "2016-05-01"))) ## 
+time + scale_x_date(limits = as.Date(c("2016-12-01", "2017-03-01"))) ## 
+time + scale_x_date(limits = as.Date(c("2017-07-01", "2017-09-01"))) ## Algar17 - constantly between Aug.1 and Aug.16 ish
+
+
+## Algar 17 is clearly a problem site -- how many occurrences do I have if I toss that site?
+Occ.no17 <- Occ_sc %>% filter(Site != "Algar17")
+
+sum(Occ_sc$Coyote) #131
+sum(Occ.no17$Coyote) #74 --> removes 57 detections
+
+## How does top model compare?
+Cno17 <- glmmTMB(Coyote~ Lynx*Snow + pOpen1750 + (1|Site), data = Occ.no17, family = "binomial")
+summary(Cno17) ## Estimates are just as poor
