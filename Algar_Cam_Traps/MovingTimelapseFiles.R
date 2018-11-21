0ad9133c73760551318971a2f89f5bac1328aca0
ETattersall <ertattersall@gmail.com>
2018-11-20 21:27:50 -0800

    Moving Timelapse images into station subdirectories

--- Algar_Cam_Traps/MovingTimelapseFiles.R
+++ Algar_Cam_Traps/MovingTimelapseFiles.R
@@ -6,7 +6,7 @@
 
 library(camtrapR)
 
-setwd("D:/Algar_Apr-Nov2017")
+setwd("D:/CameraTrap_Images/Algar_Apr-Nov2018")
 
 # List all Images in directory
 Images <- list.files('Renamed_Images', recursive = TRUE)
@@ -21,16 +21,18 @@ tail(Timelapse)
 
 ## Move Timelapse files into new folder
 ## Use small subset of images to test (created manually)
+setwd("D:/CameraTrap_Images/Algar_Apr-Nov2018")
 
-test <- list.files('Test_copy', recursive = TRUE)
-t1 <- grep(pattern = "12-00-00(1)", x = test, fixed = TRUE, value = TRUE)
+test <- list.files('Test_copy') ## Full list of images in test file
+head(test)
+t1 <- grep(pattern = "12-00-00(1)", x = test, fixed = TRUE, value = TRUE) ## List of Timelapse images in test file
 head(t1)
 class(t1)
 
 
 ## Copy files from Station folders into Timelapse and remove from original folders
 ## Set working directory to Test_copy
-setwd("D:/Algar_Apr-Nov2017/Test_copy")
+setwd("D:/CameraTrap_Images/Algar_Apr-Nov2018/Renamed_Images/Test_copy")
 #create a Timelapse folder
 createStationFolders(inDir = getwd(),
                      stations = 'Timelapse',
@@ -46,4 +48,62 @@ file.move(from = t1, to = 'Timelapse') #does not retain subdirectories
 
 summary(list.files('Timelapse')) #606, same as in t1
 summary(list.files(getwd(), recursive = TRUE)) #1295, same as test. All files kept!!
+
+
+### Move Timelapse files for entire deployment
+# Set working directory to Renamed_Images
+setwd("D:/CameraTrap_Images/Algar_Apr-Nov2018/Renamed_Images")
+
+#create a Timelapse folder
+createStationFolders(inDir = getwd(),
+                     stations = 'Timelapse',
+                     createinDir = FALSE)
+
+
+## Move files with above function
+
+file.move(from = Timelapse, to = 'Timelapse') #does not retain subdirectories
+
+summary(list.files('Timelapse'))
+summary(list.files(getwd(), recursive = TRUE)) ## All files moved
+
+
+
+
+
+
+
+
+
+
+
+#### But Timelapse folder does not have station subfolders
+setwd("D:/CameraTrap_Images/Algar_Apr-Nov2018/Renamed_Images/Timelapse")
+
+F1 <- list.files() ## List of Timelapse images without the Station subdirectories attached
+
+tail(F1)
+head(F1)
+
+### Create station folders within Timelapse to move images into
+## List of station names
+Stations <- paste("Algar", formatC(1:73, width=2, flag="0"), sep="") #Formats numbers as fixed width of 2
+
+## Create station folders
+createStationFolders(inDir = getwd(), 
+                     stations = Stations,
+                     createinDir = TRUE)
+
+head(Timelapse)
+head(F1)
+
+
+ ## Move Timelapse images into Timelapse station folders
+## from = F1, to = Timelapse (still has subdirectory)
+file.move(from = F1, to = Timelapse)
+
+getwd()
+
+summary(list.files('Timelapse', recursive = TRUE))
+
 
